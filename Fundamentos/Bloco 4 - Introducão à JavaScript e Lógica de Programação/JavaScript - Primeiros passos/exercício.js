4a340144c8ce0497a49f0e01b1c610e8fe4cf6a0
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 11:55:34 -0300

    Added function to compare a, b and c and return the highest number

--- Fundamentos/Bloco 4 - Introducão à JavaScript e Lógica de Programação/JavaScript - Primeiros passos/exercício.js
+++ Fundamentos/Bloco 4 - Introducão à JavaScript e Lógica de Programação/JavaScript - Primeiros passos/exercício.js
@@ -1,5 +1,6 @@
 const a = 1;
 const b = 2;
+const c = 3;
 
 // Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
 // Adição (a + b)
@@ -38,4 +39,19 @@ function maior(a, b) {
     return b;
   }
   return "Os números são iguais";
+}
+
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+function maior3(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  }
+  if (b > a && b > c) {
+    return b;
+  }
+  if (c > a && c > b) {
+    return c;
+  }
+  return "Dois ou mais números são iguais";
 }

a1de74a4529ea911a8116d82a446eb67dea8e740
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 12:28:48 -0300

    Added function to determine if one of three numbers is odd

--- Fundamentos/Bloco 4 - Introducão à JavaScript e Lógica de Programação/JavaScript - Primeiros passos/exercício.js
+++ Fundamentos/Bloco 4 - Introducão à JavaScript e Lógica de Programação/JavaScript - Primeiros passos/exercício.js
@@ -134,3 +134,12 @@ function parOuImpar(a, b, c) {
   return false;
 }
 
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+
+function parOuImpar(a, b, c) {
+  if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+    return true;
+  }
+  return false;
+}

85ff6bf644ba982a5b658fe6284f079adaee8568
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 15:32:43 -0300

    Added a console.log welcoming the first key-value pair of the info object

---
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte I - Objetos e For In.js
@@ -0,0 +1,13 @@
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+// Bem-vinda, Margarida
+
+console.log(`Bem-vinda, ${info.personagem}`);
+

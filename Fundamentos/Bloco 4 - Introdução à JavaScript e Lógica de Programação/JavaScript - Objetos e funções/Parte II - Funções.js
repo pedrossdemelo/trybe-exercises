f815053ff26b04a7d611ffd51825066d9f345480
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 16:39:59 -0300

    Added a function that counts each fruit instance in an array, stores it in a new object with a key of fruit and value of the amount of said fruit and then logs each fruit and its amount inside the basket array

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte II - Funções.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte II - Funções.js
@@ -212,3 +212,45 @@ function arrayOfNumbers(vector) {
 }
 
 console.log(arrayOfNumbers(vector));
+
+// A partir do array de frutas basket , retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor. Por exemplo, o array ['Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva'], deverá retornar { Melancia: 3, Abacate: 1, Uva: 1 } quando passado como argumento para a função.
+// Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: Sua cesta possui: x Melancias, x Abacates...
+
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+function countFruits(basket) {
+  let obj = {};
+  for (let i = 0; i < basket.length; i++) {
+    if (obj[basket[i]]) {
+      obj[basket[i]]++;
+    } else {
+      obj[basket[i]] = 1;
+    }
+  }
+  let msg = 'Sua cesta possui: ';
+  for (let key in obj) {
+    msg += `${obj[key]} ${key}${obj[key] > 1 ? 's' : ''}, `;
+  }
+  console.log(msg.slice(0, -2));
+  return obj;
+}
+
+console.log(countFruits(basket));

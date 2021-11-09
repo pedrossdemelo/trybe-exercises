8410512f8d31e87bd6649f01bac7e9978cfcf0b8
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 15:51:44 -0300

    Added a function that prints both values of info and info2 object keys and detects for duplicate key values

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte I - Objetos e For In.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte I - Objetos e For In.js
@@ -53,3 +53,31 @@ function showValues(obj) {
 }
 
 showValues(info);
+
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 
+// 'Tio Patinhas',
+// 'Christmas on Bear Mountain, Dell's Four Color Comics #178',
+// 'O último MacPatinhas',
+// 'Sim'.
+// Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+// Margarida e Tio Patinhas
+// Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+// Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+// Ambos recorrentes // Atenção para essa última linha!
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+function printBothValues(obj1, obj2) {
+  for (let key in obj1) {
+    if (obj1[key] === obj2[key]) {
+      console.log(`Ambos ${key}s`);
+    } else console.log(`${obj1[key]} e ${obj2[key]}`);
+  }
+}
+
+printBothValues(info, info2);

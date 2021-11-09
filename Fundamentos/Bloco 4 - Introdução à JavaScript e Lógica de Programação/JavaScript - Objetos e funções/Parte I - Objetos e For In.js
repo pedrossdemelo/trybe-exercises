8ac0ffe9bbcafe412528e82657585f8c6ae7eabe
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 15:35:43 -0300

    Added a function to add a new key value of 'recorrente' and its pair 'Sim' and console.log the new object

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte I - Objetos e For In.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte I - Objetos e For In.js
@@ -11,3 +11,17 @@ let info = {
 
 console.log(`Bem-vinda, ${info.personagem}`);
 
+// Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+// {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim'
+// }
+
+function addRecorrente(obj) {
+  obj.recorrente = 'Sim';
+  console.log(obj);
+}
+
+addRecorrente(info);

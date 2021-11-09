f60960fb75de055d342145300a2b805dffeb554d
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 12:31:27 -0300

    Added function to calculate the profits of an item

--- Fundamentos/Bloco 4 - Introducão à JavaScript e Lógica de Programação/JavaScript - Primeiros passos/exercício.js
+++ Fundamentos/Bloco 4 - Introducão à JavaScript e Lógica de Programação/JavaScript - Primeiros passos/exercício.js
@@ -143,3 +143,20 @@ function parOuImpar(a, b, c) {
   }
   return false;
 }
+
+// Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%.
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+// valorCustoTotal = valorCusto + impostoSobreOCusto
+// lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+function lucro(custo, venda) {
+  if (custo < 0 || venda < 0) {
+    return "Valor inválido";
+  }
+  const impostoSobreOCusto = custo * 0.2;
+  const valorCustoTotal = custo + impostoSobreOCusto;
+  const lucro = venda - valorCustoTotal;
+  return lucro;
+}

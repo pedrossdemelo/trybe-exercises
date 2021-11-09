cb82d7d54b0190f2a9ab4880aae55a94c9f4a55a
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 12:56:15 -0300

    Added a function to print every element in array

---
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript -  Array e loop For/exercício.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+
+function printArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}

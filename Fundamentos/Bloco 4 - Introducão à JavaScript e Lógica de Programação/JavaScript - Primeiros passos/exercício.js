3993cd83f2aca33bf1eb63b9d7e987ffdb6526c8
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 11:49:00 -0300

    Added functions for sum, subtraction, multiplication, division and module

---
+++ Fundamentos/Bloco 4 - Introducão à JavaScript e Lógica de Programação/JavaScript - Primeiros passos/exercício.js
@@ -0,0 +1,22 @@
+const a = 1;
+const b = 2;
+
+function soma(a, b) {
+  return a + b;
+}
+
+function subtracao(a, b) {
+  return a - b;
+}
+
+function multiplicacao(a, b) {
+  return a * b;
+}
+
+function divisao(a, b) {
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}

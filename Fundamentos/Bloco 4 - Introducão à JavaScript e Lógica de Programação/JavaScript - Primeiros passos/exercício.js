c9e6efdbdeef3d3b804c44d08e8403180753a6bb
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 12:09:06 -0300

    Added function to validate triangle angles

--- Fundamentos/Bloco 4 - Introducão à JavaScript e Lógica de Programação/JavaScript - Primeiros passos/exercício.js
+++ Fundamentos/Bloco 4 - Introducão à JavaScript e Lógica de Programação/JavaScript - Primeiros passos/exercício.js
@@ -67,3 +67,16 @@ function positivoOuNegativo(a) {
   }
   return "zero";
 }
+
+// Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+function angulosTriangulo(a, b, c) {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return "Ângulo inválido";
+  }
+  if (a + b + c === 180) {
+    return true;
+  }
+  return false;
+}
+

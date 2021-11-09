280603fe449007978540b87af30db9db25940ff5
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 15:06:25 -0300

    Bonus 2: Added a function that outputs true if the input number is prime

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Lógica de Programação e Algoritmos/exercício.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Lógica de Programação e Algoritmos/exercício.js
@@ -78,3 +78,18 @@ function asteriskPyramidEmpty(n) {
     }
   }
 }
+
+// Faça um programa que diz se um número definido numa variável é primo ou não.
+// Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
+// Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+
+function isPrime(n) {
+  let isPrime = true;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  return isPrime;
+}

7ade9f20e988cda13216da757789dc849ac00036
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 16:12:24 -0300

    Added a function that checks if the ending of a specified string is equal to the specified second string argument

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte II - Funções.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte II - Funções.js
@@ -109,3 +109,27 @@ function somatorioDeNumerosDe1AteN(n) {
 }
 
 console.log(somatorioDeNumerosDe1AteN(5));
+
+// Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+// Valor de teste: 'trybe' e 'be'
+// Valor esperado no retorno da função: true
+// verificaFimPalavra('trybe', 'be') ;
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan') ;
+// Retorno esperado: false
+
+function verificaFimPalavra(word, ending) {
+  let fimPalavra = false;
+  for (let i = 0; i < ending.length; i++) {
+    if (word[word.length - 1 - i] === ending[ending.length - 1 - i]) {
+      fimPalavra = true;
+    } else {
+      fimPalavra = false;
+      break;
+    }
+  }
+  return fimPalavra;
+}
+
+console.log(verificaFimPalavra('trybe', 'be'));
+console.log(verificaFimPalavra('joaofernando', 'fernan'));

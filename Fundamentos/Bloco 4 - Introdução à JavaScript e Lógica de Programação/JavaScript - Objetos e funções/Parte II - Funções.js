67f23ed95a859dbc25bd92046e1b936e85bc707e
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 16:04:14 -0300

    Added a function that returns the index of the highest value elemnet in an input array

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte II - Funções.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte II - Funções.js
@@ -19,3 +19,21 @@ function verificaPalindrome(palavra) {
 
 console.log(verificaPalindrome('arara'));
 console.log(verificaPalindrome('desenvolvimento'));
+
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// Array de teste: [2, 3, 6, 7, 10, 1]; .
+// Valor esperado no retorno da função: 4 .
+
+function retornaIndiceMaiorValor(array) {
+  let maiorValor = 0;
+  let indiceMaiorValor = 0;
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] > maiorValor) {
+          maiorValor = array[i];
+          indiceMaiorValor = i;
+      }
+  }
+  return indiceMaiorValor;
+}
+
+console.log(retornaIndiceMaiorValor([2, 3, 6, 7, 10, 1]));

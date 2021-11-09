2cc142ae996f031eb96eccba6342b2a9d9b2402c
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-09 15:54:41 -0300

    Added  a function to print the favorite book of said reader in an object

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte I - Objetos e For In.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Parte I - Objetos e For In.js
@@ -81,3 +81,26 @@ function printBothValues(obj1, obj2) {
 }
 
 printBothValues(info, info2);
+
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+// Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+function printFavoriteBook(obj) {
+  console.log(`O livro favorito de ${obj.nome} ${obj.sobrenome} se chama '${obj.livrosFavoritos[0].titulo}'`);
+}
+
+printFavoriteBook(leitor);

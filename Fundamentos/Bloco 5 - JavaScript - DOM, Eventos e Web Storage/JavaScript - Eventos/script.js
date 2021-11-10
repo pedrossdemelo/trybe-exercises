bffa9a2556421cd5fcdac5c2423d1c17335db4c5
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-10 13:57:56 -0300

    Aded a function to add a task status indicator to the div with my-tasks class

--- Fundamentos/Bloco 5 - JavaScript - DOM, Eventos e Web Storage/JavaScript - Eventos/script.js
+++ Fundamentos/Bloco 5 - JavaScript - DOM, Eventos e Web Storage/JavaScript - Eventos/script.js
@@ -128,3 +128,16 @@ function addCustomTask(string) {
   myTasks.appendChild(customTask);
 }
 addCustomTask('teste');
+
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+function addCustomTaskColor(string) {
+  const myTasks = document.getElementsByClassName('my-tasks')[0];
+  const customTask = document.createElement('div');
+  customTask.classList.add('task');
+  customTask.style.backgroundColor = string;
+  myTasks.appendChild(customTask);
+}
+addCustomTaskColor('red');

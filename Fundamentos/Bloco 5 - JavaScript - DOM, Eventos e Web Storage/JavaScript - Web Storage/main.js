7405bdf4724025766cb3f4d65667a185bf4f0648
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-10 21:53:00 -0300

    Added buttons to html and css

--- Fundamentos/Bloco 5 - JavaScript - DOM, Eventos e Web Storage/JavaScript - Web Storage/main.js
+++ Fundamentos/Bloco 5 - JavaScript - DOM, Eventos e Web Storage/JavaScript - Web Storage/main.js
@@ -11,49 +11,49 @@
 // Bônus
 // As propriedades descritas acima são obrigatórias , mas você é livre para adicionar qualquer outra propriedade que julgar válida e que tenha como objetivo a melhora da experiência da pessoa que lê em sua página.
 
-function changeBackgroundColor() {
-    document.body.style.backgroundColor = "white";
-    document.getElementsByTagName('header')[0].style.backgroundColor = "#FAFAFA";
-    document.getElementsByTagName('footer')[0].style.backgroundColor = "#FAFAFA";
-    document.getElementById('top-nav').style.backgroundColor = "#FAFAFA";
-}
+// function changeBackgroundColor() {
+//     document.body.style.backgroundColor = "white";
+//     document.getElementsByTagName('header')[0].style.backgroundColor = "#FAFAFA";
+//     document.getElementsByTagName('footer')[0].style.backgroundColor = "#FAFAFA";
+//     document.getElementById('top-nav').style.backgroundColor = "#FAFAFA";
+// }
 
-changeBackgroundColor();
+// changeBackgroundColor();
 
-function changeTextColor() {
-  document.body.style.color = "red";
-  const a = document.getElementsByTagName('a');
-  [...a].forEach(a => a.style.color = "red");
-}
+// function changeTextColor() {
+//   document.body.style.color = "red";
+//   const a = document.getElementsByTagName('a');
+//   [...a].forEach(a => a.style.color = "red");
+// }
 
-changeTextColor();
+// changeTextColor();
 
-function changeFontSize() {
-  // default is 16px
-  document.body.style.fontSize = "18px";
-  // default is 14px
-  document.getElementById('location').style.fontSize = "16px"
-}
+// function changeFontSize() {
+//   // default is 16px
+//   document.body.style.fontSize = "18px";
+//   // default is 14px
+//   document.getElementById('location').style.fontSize = "16px"
+// }
 
-changeFontSize();
+// changeFontSize();
 
-function changeLineHeight() {
-  // default is 1.3
-  document.body.style.lineHeight = "1.3";
-}
+// function changeLineHeight() {
+//   // default is 1.3
+//   document.body.style.lineHeight = "1.3";
+// }
 
-changeLineHeight();
+// changeLineHeight();
 
-function changeFontFamily() {
-  document.body.style.fontFamily = "Arial";
-  const h1 = document.getElementsByTagName('h1');
-  [...h1].forEach(h1 => h1.style.fontFamily = "Times New Roman");
-  const h2 = document.getElementsByTagName('h2');
-  [...h2].forEach(h2 => h2.style.fontFamily = "Times New Roman");
-  const h3 = document.getElementsByTagName('h3');
-  [...h3].forEach(h3 => h3.style.fontFamily = "Times New Roman");
-  const navItems = document.querySelectorAll('#nav-items li a');
-  [...navItems].forEach(navItem => navItem.style.fontFamily = "Times New Roman");
-}
+// function changeFontFamily() {
+//   document.body.style.fontFamily = "Arial";
+//   const h1 = document.getElementsByTagName('h1');
+//   [...h1].forEach(h1 => h1.style.fontFamily = "Times New Roman");
+//   const h2 = document.getElementsByTagName('h2');
+//   [...h2].forEach(h2 => h2.style.fontFamily = "Times New Roman");
+//   const h3 = document.getElementsByTagName('h3');
+//   [...h3].forEach(h3 => h3.style.fontFamily = "Times New Roman");
+//   const navItems = document.querySelectorAll('#nav-items li a');
+//   [...navItems].forEach(navItem => navItem.style.fontFamily = "Times New Roman");
+// }
 
-changeFontFamily();
+// changeFontFamily();

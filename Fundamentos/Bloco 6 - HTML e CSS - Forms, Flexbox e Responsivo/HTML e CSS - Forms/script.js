cf7871fc273f52c36d332483298e09cdbde61f85
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-12 09:21:14 -0300

    Added a fieldset with personal data

---
+++ Fundamentos/Bloco 6 - HTML e CSS - Forms, Flexbox e Responsivo/HTML e CSS - Forms/script.js
@@ -0,0 +1,70 @@
+// add all brazillian states as options to the element with id 'estado'
+function addEstados() {
+  let states = [
+    'Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins'
+  ];
+  let statesAcronyms = [
+    'AC',
+    'AL',
+    'AP',
+    'AM',
+    'BA',
+    'CE',
+    'DF',
+    'ES',
+    'GO',
+    'MA',
+    'MT',
+    'MS',
+    'MG',
+    'PA',
+    'PB',
+    'PR',
+    'PE',
+    'PI',
+    'RJ',
+    'RN',
+    'RS',
+    'RO',
+    'RR',
+    'SC',
+    'SP',
+    'SE',
+    'TO'
+  ];
+  let selectState = document.getElementById('estado');
+  for (let i = 0; i < states.length; i++) {
+      let option = document.createElement('option');
+      option.innerHTML = states[i];
+      option.value = statesAcronyms[i];
+      selectState.appendChild(option);
+  }
+}
+
+addEstados();

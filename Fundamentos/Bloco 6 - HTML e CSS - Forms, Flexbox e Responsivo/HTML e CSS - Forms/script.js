2a1292da4fdca291329256985504068516d3da8d
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-12 11:08:04 -0300

    Added a fieldset with last employment details and implemented a date validation function

--- Fundamentos/Bloco 6 - HTML e CSS - Forms, Flexbox e Responsivo/HTML e CSS - Forms/script.js
+++ Fundamentos/Bloco 6 - HTML e CSS - Forms, Flexbox e Responsivo/HTML e CSS - Forms/script.js
@@ -59,12 +59,39 @@ function addEstados() {
     'TO'
   ];
   let selectState = document.getElementById('estado');
-  for (let i = 0; i < states.length; i++) {
+  for (let index = 0; index < states.length; index++) {
       let option = document.createElement('option');
-      option.innerHTML = states[i];
-      option.value = statesAcronyms[i];
+      option.innerHTML = states[index];
+      option.value = statesAcronyms[index];
       selectState.appendChild(option);
   }
 }
 
 addEstados();
+
+function handleSubmit(event) {
+  let data = document.getElementById('data');
+  if (data.value === '') {
+    alert('Preencha o campo data');
+  }
+  let dataSplit = data.value.split('/');
+  let dia = Number(dataSplit[0]);
+  let mes = Number(dataSplit[1]);
+  let ano = Number(dataSplit[2]);
+  let dataValida = true;
+  console.log(dia, mes, ano);
+  if (dia < 1 || dia > 31 || !dia) {
+    dataValida = false;
+  }
+  if (mes < 1 || mes > 12 || !mes) {
+    dataValida = false;
+  }
+  if (ano < 0 || !ano) {
+    dataValida = false;
+  }
+  if (!dataValida) {
+    alert('Data inválida');
+  }
+}
+
+document.getElementById('submit').addEventListener('click', handleSubmit);

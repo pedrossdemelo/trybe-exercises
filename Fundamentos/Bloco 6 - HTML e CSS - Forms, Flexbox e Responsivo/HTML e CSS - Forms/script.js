42fe89ffd537f8d885b6bb10d2eb54cddfed6aff
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-12 12:09:41 -0300

    Created a function that creates a div with all the data filled in the forms

--- Fundamentos/Bloco 6 - HTML e CSS - Forms, Flexbox e Responsivo/HTML e CSS - Forms/script.js
+++ Fundamentos/Bloco 6 - HTML e CSS - Forms, Flexbox e Responsivo/HTML e CSS - Forms/script.js
@@ -69,17 +69,13 @@ function addEstados() {
 
 addEstados();
 
-function handleSubmit(event) {
+function isDataValid() {
   let data = document.getElementById('data');
-  if (data.value === '') {
-    alert('Preencha o campo data');
-  }
   let dataSplit = data.value.split('/');
   let dia = Number(dataSplit[0]);
   let mes = Number(dataSplit[1]);
   let ano = Number(dataSplit[2]);
   let dataValida = true;
-  console.log(dia, mes, ano);
   if (dia < 1 || dia > 31 || !dia) {
     dataValida = false;
   }
@@ -89,8 +85,66 @@ function handleSubmit(event) {
   if (ano < 0 || !ano) {
     dataValida = false;
   }
-  if (!dataValida) {
-    alert('Data inválida');
+  return dataValida;
+}
+
+function areFieldsFilled() {
+  let result = true;
+  let inputs = document.querySelectorAll('input');
+  for (let index = 0; index < inputs.length; index++) {
+    console.log(inputs[index]);
+    if (inputs[index].value === '') {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+areFieldsFilled();
+function handleSubmit(event) {
+  event.preventDefault();
+  let div = document.createElement('div');
+  let nome = document.getElementById('nome').value;
+  let email = document.getElementById('email').value;
+  let cpf = document.getElementById('cpf').value;
+  let endereco = document.getElementById('endereco').value;
+  let cidade = document.getElementById('cidade').value;
+  let estado = document.getElementById('estado').value;
+  let tipo = document.getElementById('tipo').value;
+  let resumo = document.getElementById('resumo').value;
+  let cargo = document.getElementById('cargo').value;
+  let descricao = document.getElementById('descricao').value;
+  let data = document.getElementById('data').value;
+  let divDados = document.createElement('div');
+  console.log(areFieldsFilled());
+  if (areFieldsFilled()) {
+    if (isDataValid()) {
+      divDados.innerHTML = `
+        <p>Nome: ${nome}</p>
+        <p>Email: ${email}</p>
+        <p>CPF: ${cpf}</p>
+        <p>Endereço: ${endereco}</p>
+        <p>Cidade: ${cidade}</p>
+        <p>Estado: ${estado}</p>
+        <p>Tipo: ${tipo}</p>
+        <p>Resumo: ${resumo}</p>
+        <p>Cargo: ${cargo}</p>
+        <p>Descrição: ${descricao}</p>
+        <p>Data: ${data}</p>
+      `;
+      div.appendChild(divDados);
+      document.body.appendChild(div);
+    }
+    if (!isDataValid()) {
+      divDados.innerHTML = `<p>Data inválida</p>`;
+      div.appendChild(divDados);
+      document.body.appendChild(div);
+      alert('Data inválida');
+    }
+  } else {
+    divDados.innerHTML = `<p>Preencha todos os campos</p>`;
+    div.appendChild(divDados);
+    document.body.appendChild(div);
   }
 }
 

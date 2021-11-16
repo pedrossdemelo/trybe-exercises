859e7a2914a288bb76037098c9d50e8bcff926c3
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-15 23:22:50 -0300

    Created a function that returns the longest word in an array

--- Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step2.js
+++ Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step2.js
@@ -1,3 +1,7 @@
 const factorial = n => n <= 1 ? 1 : n * factorial(n - 1);
 
 console.log(factorial(4));
+
+const longestWord = str => str.split(' ').reduce((acc, word) => word.length > acc.length ? word : acc);
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")) // retorna 'aconteceu'

539742b7d99a6b976076a27d1d40e9f522c28266
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-15 23:44:00 -0300

    Created a function that replaces the X in a string with a paramater

--- Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step2.js
+++ Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step2.js
@@ -5,3 +5,7 @@ console.log(factorial(4));
 const longestWord = str => str.split(' ').reduce((acc, word) => word.length > acc.length ? word : acc);
 
 console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")) // retorna 'aconteceu'
+
+const replaceX = (str, newStr) => str.replace(/ x /g, ` ${newStr} `);
+
+console.log(replaceX("Tryber x aqui!", "Bebeto"));

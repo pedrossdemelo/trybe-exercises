93bed3ec7306cf7f7bcfd7608e7815b6adbeb62b
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-15 21:55:14 -0300

    Changed string concatenation to template literal

--- Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step1.js
+++ Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step1.js
@@ -1,7 +1,7 @@
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
     console.log(ifScope);
   } else {
     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';

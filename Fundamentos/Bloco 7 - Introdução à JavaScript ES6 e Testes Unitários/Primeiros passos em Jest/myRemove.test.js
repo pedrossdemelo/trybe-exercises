584da55a0644c386f85c402818cf809d837a35e5
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-16 18:31:32 -0300

    Added a function to remove item from array and wrote a test to validate it

---
+++ Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/Primeiros passos em Jest/myRemove.test.js
@@ -0,0 +1,8 @@
+const { describe } = require('jest-circus');
+const myRemove = require('./myRemove');
+
+describe('Remove item from array', () => {
+  test('Should return [1, 2, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+});

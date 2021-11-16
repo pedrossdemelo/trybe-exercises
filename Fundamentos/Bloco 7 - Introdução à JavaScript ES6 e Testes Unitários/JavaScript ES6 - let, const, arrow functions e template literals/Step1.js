5222c1dd366a2f7ade5bbc9d019af018aca33382
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-15 22:01:32 -0300

    Added base array for second part

--- Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step1.js
+++ Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step1.js
@@ -10,3 +10,9 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉

3b4eac59510047f2723cead324a6e4a28fe2a84c
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-16 18:32:31 -0300

    Tested if myRemove() doesnt return the same array

--- Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/Primeiros passos em Jest/myRemove.test.js
+++ Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/Primeiros passos em Jest/myRemove.test.js
@@ -5,4 +5,7 @@ describe('Remove item from array', () => {
   test('Should return [1, 2, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
+  test('Should not return [1, 2, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
 });

f7131519ee7e3c67dcc437838899e12377925f91
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-15 21:52:26 -0300

    Made sure the variables inside if statements cant be used elsewhere

--- Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step1.js
+++ Fundamentos/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Step1.js
@@ -1,13 +1,12 @@
 function testingScope(escopo) {
   if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
     console.log(ifScope);
   } else {
-    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 }
 
 testingScope(true);

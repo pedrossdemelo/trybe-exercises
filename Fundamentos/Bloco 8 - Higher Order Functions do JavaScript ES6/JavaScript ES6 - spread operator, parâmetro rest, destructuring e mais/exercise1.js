b361675898d154a3ed7c4caeacf7f90109b7388b
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-17 20:16:22 -0300

    Added a function to format string based on object using destructured objects as parameters

--- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - spread operator, parâmetro rest, destructuring e mais/exercise1.js
+++ Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - spread operator, parâmetro rest, destructuring e mais/exercise1.js
@@ -11,3 +11,22 @@ rectangles.forEach((rectangle) => {
 });
 
 const sum = (...numbers) => numbers.reduce((acc, curr) => acc + curr);
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'

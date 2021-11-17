22ef9de699799f0703d8e43d5929c44825cc09a3
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-17 20:44:57 -0300

    Added a function to format a string based on an object parameter using object destructuring

--- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - spread operator, parâmetro rest, destructuring e mais/exercise1.js
+++ Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - spread operator, parâmetro rest, destructuring e mais/exercise1.js
@@ -71,4 +71,26 @@ const chiron = ['Chiron', 'Bugatti', 2016];
 
 const toObject = ([name, brand, year]) => ({name, brand, year});
 
-console.log(toObject(palio));
+const ships = [
+  {
+    name: 'Titanic',
+    length: 269.1,
+    measurementUnit: 'meters',
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+];
+
+const shipLength = ({name, length, measurementUnit}) => `${name} is ${length} ${measurementUnit} long`;
+
+console.log(shipLength(ships[0])) // 'Titanic is 269.1 meters long'
+console.log(shipLength(ships[1])) // 'Queen Mary 2 is 1132 feet long'
+console.log(shipLength(ships[2])) // 'Yamato is 256 meters long'

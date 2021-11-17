66375ef93de042b357c345ea2469990de005a120
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-17 08:09:49 -0300

    Added function to get mage damage and current mana

--- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Introdução a Higher Order Functions/bonusExercise.js
+++ Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Introdução a Higher Order Functions/bonusExercise.js
@@ -23,3 +23,18 @@ const battleMembers = { mage, warrior, dragon };
 const getDragonDamage = (dragon) => Math.floor(Math.random() * (dragon.strength - 15) + 15);
 
 const getWarriorDamage = (warrior) => Math.floor(Math.random() * (warrior.strength * warrior.weaponDmg - warrior.strength) + warrior.strength);
+
+const getMageDamage = (mage) => {
+  const damage = Math.floor(Math.random() * (mage.intelligence * 2 - mage.intelligence) + mage.intelligence);
+  const manaCost = 15;
+  if (mage.mana < manaCost) {
+    return {
+      damage: 'Não possui mana suficiente',
+      mana: mage.mana,
+    };
+  }
+  return {
+    damage: damage,
+    mana: mage.mana - manaCost,
+  };
+}

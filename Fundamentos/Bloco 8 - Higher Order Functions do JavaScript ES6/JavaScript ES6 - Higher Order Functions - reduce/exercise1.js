5b5f7daf38f31f4c70e9d0a55472921c8e01768d
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-17 14:10:05 -0300

    Added function to flatten an array of arrays

--- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Higher Order Functions - reduce/exercise1.js
+++ Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Higher Order Functions - reduce/exercise1.js
@@ -4,6 +4,6 @@ const arrays = [
   [4, 5, 6],
 ];
 
-function flatten() {
-  // escreva seu código aqui
+function flatten(arrays) {
+  return arrays.reduce((acc, array) => acc.concat(array), []);
 }

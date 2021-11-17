d88a09c58ecda15c8b35166b1614c5144892f623
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-17 07:20:10 -0300

    Added base exercise file

---
+++ Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Introdução a Higher Order Functions/exercise.js
@@ -0,0 +1,8 @@
+const newEmployees = () => {
+  const employees = {
+    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};

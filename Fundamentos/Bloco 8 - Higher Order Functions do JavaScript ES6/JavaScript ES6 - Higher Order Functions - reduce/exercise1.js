f069945c528fc66cc0573b2f8d00a2a233851a1a
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-17 15:10:40 -0300

    Added function to return an object with the grade average of every student in an array

--- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Higher Order Functions - reduce/exercise1.js
+++ Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Higher Order Functions - reduce/exercise1.js
@@ -84,3 +84,11 @@ const names = [
 ];
 
 const containsA = (names) => names.reduce((acc, name) => (acc + name.toLowerCase().split('a').length - 1), 0);
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const studentAverage = (students, grades) => students.map((student, index) => ({
+  name: student,
+  average: grades[index].reduce((acc, grade) => (acc + grade), 0) / grades[index].length,
+}));

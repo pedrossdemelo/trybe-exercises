7a34a7e1d9518121a2f26e589d2d3acd67fbca13
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-17 07:25:06 -0300

    Added function as argument to newEmployees()

--- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Introdução a Higher Order Functions/exercise.js
+++ Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Introdução a Higher Order Functions/exercise.js
@@ -1,8 +1,12 @@
-const newEmployees = () => {
+const returnName = (name) => name;
+
+const newEmployees = (f) => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: f('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: f('Luiza Drummond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: f('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
+
+console.log(newEmployees(returnName));

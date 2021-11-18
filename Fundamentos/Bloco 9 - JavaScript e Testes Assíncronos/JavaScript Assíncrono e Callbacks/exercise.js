c508f2f1aeb5ba0ffd6cd22937da52cb9c2cd106
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-18 13:03:16 -0300

    Added callback function as argument to to sendMarsTemperature

--- Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono e Callbacks/exercise.js
+++ Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono e Callbacks/exercise.js
@@ -5,12 +5,20 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-const sendMarsTemperature = () => {
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const sendMarsTemperature = (callBack) => {
   setTimeout(() => {
     const temperature = getMarsTemperature();
-    const msg = `Mars temperature is: ${temperature} degree Celsius`;
-    console.log(msg);
+    callBack(temperature);
   }, messageDelay());
 };
 
-sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo

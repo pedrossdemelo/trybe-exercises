6e8ac1fa09a6e2e6d3fc3ca366c077f96f8edbad
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-18 20:56:17 -0300

    Applied joke to jokeContainer h2

--- Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/apiScript.js
+++ Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/apiScript.js
@@ -1,15 +1,15 @@
 // apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
+const fetchJoke = async () => {
   const myObject = {
     method: 'GET',
     headers: { 'Accept': 'application/json' }
   };
 
-  fetch(API_URL, myObject)
+  const joke = fetch(API_URL, myObject)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => document.getElementById('jokeContainer').innerHTML = data.joke);
 };
 
 window.onload = () => fetchJoke();

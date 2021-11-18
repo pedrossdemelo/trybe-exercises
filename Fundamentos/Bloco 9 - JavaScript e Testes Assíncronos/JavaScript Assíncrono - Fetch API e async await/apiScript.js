2f95ca360a472bcc2c530f250c3270bf82de4a99
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-18 20:47:13 -0300

    Added fetch params

--- Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/apiScript.js
+++ Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/apiScript.js
@@ -2,7 +2,12 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject);
 };
 
 window.onload = () => fetchJoke();

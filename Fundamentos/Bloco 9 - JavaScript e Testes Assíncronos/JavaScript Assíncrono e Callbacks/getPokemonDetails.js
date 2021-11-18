b6fe20f4f45fc47d4c3e96c4e25cece51417f622
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-18 17:12:58 -0300

    Added tests to validate getPokemonDetails

--- Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono e Callbacks/getPokemonDetails.js
+++ Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono e Callbacks/getPokemonDetails.js
@@ -16,30 +16,19 @@ const pokemons = [
   },
 ];
 
-const filter = (pokemon) => pokemon.name === 'Bulbasaur';
-
-const printMessage = (error, message) => {
-  if (error) throw error;
-  else console.log(message);
-};
-
 function getPokemonDetails(filter, callback) {
-  setTimeout(() => {
-    if (pokemons.find(filter) === undefined) {
-      console.log('Pokemon not found');
+    if (pokemons.find(pokemon => pokemon.name === filter) === undefined) {
       return callback(new Error('Não temos esse pokémon para você :('), null);
     }
-    const pokemon = pokemons.find(filter);
+    const pokemon = pokemons.find(pokemon => pokemon.name === filter);
 
     const { name, type, ability } = pokemon;
 
     const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
 
     callback(null, messageFromProfOak);
-  }, 2000);
 }
 
-getPokemonDetails(filter, printMessage);
 
 module.exports = {
   getPokemonDetails,

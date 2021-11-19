e7b0679c7d7c7c92ef3a83319b62773f993552f3
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-19 00:27:58 -0300

    Converted usd to brl using an API

--- Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/api.js
+++ Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/api.js
@@ -1,14 +1,18 @@
 const API_URL = 'https://api.coincap.io/v2/assets/';
 
+const CONVERSION_URL = 'https://economia.awesomeapi.com.br/last/USD-BRL' 
+
 const coinList = document.getElementById('coin-list');
 
 const getCoinArray = async () => {
   let top10Coins = await fetch(API_URL)
   .then(response => response.json())
   .then(response => response.data.slice(0, 10));
-  console.log(top10Coins);
+  let usdToBRL = await fetch(CONVERSION_URL)
+  .then(response => response.json())
+  .then(response => response.USDBRL.ask);
   top10Coins.forEach((coin, index) => {
-    coinList.innerHTML += `<li>${index + 1}. ${coin.name} (${coin.symbol}): ${Number(coin.priceUsd).toFixed(2)} USD <span class="${Number(coin.changePercent24Hr) < 0 ? 'bear' : 'bull'}">(${Number(coin.changePercent24Hr).toFixed(2)}%)</span></li>`;
+    coinList.innerHTML += `<li>${index + 1}. ${coin.name} (${coin.symbol}): ${(Number(coin.priceUsd) * Number(usdToBRL)).toFixed(2)} BRL <span class="${Number(coin.changePercent24Hr) < 0 ? 'bear' : 'bull'}">(${Number(coin.changePercent24Hr).toFixed(2)}%)</span></li>`;
   });
 }
 

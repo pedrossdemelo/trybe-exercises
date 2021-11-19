4a63a501816df123d1fcfb851664789ecb2cedad
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-18 23:50:32 -0300

    Styled the website

--- Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/api.js
+++ Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/api.js
@@ -7,8 +7,8 @@ const getCoinArray = async () => {
   .then(response => response.json())
   .then(response => response.data.slice(0, 10));
   console.log(top10Coins);
-  top10Coins.forEach(coin => {
-    coinList.innerHTML += `<li>${coin.name} (${coin.symbol}): ${Number(coin.priceUsd).toFixed(2)} (${Number(coin.changePercent24Hr).toFixed(2)}%)</li>`;
+  top10Coins.forEach((coin, index) => {
+    coinList.innerHTML += `<li>${index + 1}. ${coin.name} (${coin.symbol}): ${Number(coin.priceUsd).toFixed(2)} USD <span class="${Number(coin.changePercent24Hr) < 0 ? 'bear' : 'bull'}">(${Number(coin.changePercent24Hr).toFixed(2)}%)</span></li>`;
   });
 }
 

91f072aff5c1ff5b9a29afa01e5a695a243a5c73
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-19 16:47:52 -0300

    Added tests to assert received array contains certain repos

--- Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/Jest - Testes Assíncronos/githubApi.test.js
+++ Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/Jest - Testes Assíncronos/githubApi.test.js
@@ -2,10 +2,15 @@
 
 const fetch = require('node-fetch');
 
+const githubUrl = 'https://api.github.com/orgs/tryber/repos?per_page=100';
+
 const getRepos = (url) => (
   fetch(url)
     .then((response) => response.json())
     .then((data) => data.map((repo) => repo.name))
 );
 
-// implemente seus testes aqui
+test('Testando se a API retorna certos repos', async () => {
+  const repos = await getRepos(githubUrl);
+  expect(repos).toEqual(expect.arrayContaining(['sd-01-week10-movie-card-library-tests', 'sd-02-week11-movie-card-library-tests']));
+})

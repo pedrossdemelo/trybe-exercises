466f161955009c764bac78e378e18969aa99dfff
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-18 22:20:16 -0300

    Fetched the top 10 coins by market cap

--- Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/api.js
+++ Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/JavaScript Assíncrono - Fetch API e async await/api.js
@@ -3,7 +3,13 @@ const API_URL = 'https://api.coincap.io/v2/assets/';
 const coinList = document.getElementById('coin-list');
 
 const getCoinArray = async () => {
-
+  let top10Coins = await fetch(API_URL)
+  .then(response => response.json())
+  .then(response => response.data.slice(0, 10));
+  console.log(top10Coins);
+  top10Coins.forEach(coin => {
+    coinList.innerHTML += `<li>${coin.name} (${coin.symbol}): ${Number(coin.priceUsd).toFixed(2)} (${Number(coin.changePercent24Hr).toFixed(2)}%)</li>`;
+  });
 }
 
 getCoinArray();

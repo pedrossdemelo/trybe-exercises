3735cf2fbbf93585015d7d3813e39444f91bd671
Pedro Sousa <pedrossm2000@gmail.com>
2021-11-19 15:24:35 -0300

    Added base exercise code

---
+++ Fundamentos/Bloco 9 - JavaScript e Testes Assíncronos/Jest - Testes Assíncronos/githubApi.test.js
@@ -0,0 +1,11 @@
+// githubApi.test.js
+
+const fetch = require('node-fetch');
+
+const getRepos = (url) => (
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.map((repo) => repo.name))
+);
+
+// implemente seus testes aqui

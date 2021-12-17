57555c48bb9745da7d3ed2f5a52e4fa66abb989c
Pedro Sousa <pedrossm2000@gmail.com>
2021-12-17 17:34:48 -0300

    Added Task element to app

--- Front End/Bloco 10 - Introdução à React/Hello World no React/app/src/App.js
+++ Front End/Bloco 10 - Introdução à React/Hello World no React/app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Task = ({value}) => {
+  return (
+    <p>{value}</p>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <Task value="Hello World" />
+      </ul>
     </div>
   );
 }

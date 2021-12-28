d084aed8e7fc3143f8c2e1ab5c9da60c596e5b6f
Pedro Sousa <pedrossm2000@gmail.com>
2021-12-28 04:51:52 -0300

    Added state to determine what pokemon is shown on screen

--- Front End/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/Componentes com estado e eventos/my-pokedex/src/App.js
+++ Front End/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/Componentes com estado e eventos/my-pokedex/src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 import React from 'react';
 import pokemons from './data.js'
 
+function Pokedex(props) {
+  // Altere a sua página para que, ao invés de exibir toda a lista de pokémons, ela exiba um pokémon por vez. Ao apertar um botão de Próximo pokémon, a página passa a exibir o próximo pokémon da lista, e depois o próximo, e assim sucessivamente. Ao se chegar ao último pokémon da lista, a pokedex deve voltar para o primeiro pokémon no apertar do botão.
+
+  const [pokemon, setPokemon] = React.useState(pokemons[0]);
+  return (
+    <div>
+      <h1>{pokemon.name}</h1>
+      <img src={pokemon.image} alt={pokemon.name} />
+      <p>{pokemon.type}</p>
+      <button onClick={() => setPokemon(pokemons[(pokemons.indexOf(pokemon) + 1) % pokemons.length])}>Next</button>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      {pokemons.map(pokemon => (
-        <div key={pokemon.id}>
-          <h1>{pokemon.name}</h1>
-          <img src={pokemon.image} alt={pokemon.name} />
-          <p>{pokemon.type}</p>
-          <p>{pokemon.averageWeight.value} {pokemon.averageWeight.measurementUnit}</p>
-          <a href={pokemon.moreInfo}>More Info</a>
-        </div>
-      ))}
+      <Pokedex />
     </div>
   );
 }

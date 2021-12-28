a49e714734d7305b13242b09ce3f81c31850b153
Pedro Sousa <pedrossm2000@gmail.com>
2021-12-28 18:04:39 -0300

    Added filters

--- Front End/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/Componentes com estado e eventos/my-pokedex/src/App.js
+++ Front End/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/Componentes com estado e eventos/my-pokedex/src/App.js
@@ -3,15 +3,29 @@ import React from 'react';
 import pokemons from './data.js'
 
 function Pokedex(props) {
-  // Altere a sua página para que, ao invés de exibir toda a lista de pokémons, ela exiba um pokémon por vez. Ao apertar um botão de Próximo pokémon, a página passa a exibir o próximo pokémon da lista, e depois o próximo, e assim sucessivamente. Ao se chegar ao último pokémon da lista, a pokedex deve voltar para o primeiro pokémon no apertar do botão.
-
+  const [pokemonsList, setPokemonsList] = React.useState(pokemons);
   const [pokemon, setPokemon] = React.useState(pokemons[0]);
   return (
     <div>
-      <h1>{pokemon.name}</h1>
+      <h2>{pokemon.name}</h2>
       <img src={pokemon.image} alt={pokemon.name} />
+      {/* Add more info */}
+      <p>Average weight: {pokemon.averageWeight.value} {pokemon.averageWeight.measurementUnit}</p>
       <p>{pokemon.type}</p>
-      <button onClick={() => setPokemon(pokemons[(pokemons.indexOf(pokemon) + 1) % pokemons.length])}>Next</button>
+      <a href={pokemon.moreInfo}>More info</a>
+      <button onClick={() => setPokemon(pokemonsList[(pokemonsList.indexOf(pokemon) + 1) % pokemonsList.length])}>Next</button>
+      <button onClick={() => {
+        setPokemonsList(pokemons.filter(pokemon => pokemon.type === 'Fire'));
+        setPokemon(pokemons.find(pokemon => pokemon.type === 'Fire'));
+      }}>Fire</button>
+      <button onClick={() => {
+        setPokemonsList(pokemons.filter(pokemon => pokemon.type === 'Psychic'));
+        setPokemon(pokemons.find(pokemon => pokemon.type === 'Psychic'));
+      }}>Psychic</button>
+      <button onClick={() => {
+        setPokemonsList(pokemons);
+        setPokemon(pokemons[0]);
+      }}>Reset</button>
     </div>
   );
 }

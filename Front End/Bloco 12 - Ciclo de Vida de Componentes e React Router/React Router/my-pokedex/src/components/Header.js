9702fc919334aa2615b503022327c8e7204d75a6
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-05 02:06:32 -0300

    Added all filters

--- Front End/Bloco 12 - Ciclo de Vida de Componentes e React Router/React Router/my-pokedex/src/components/Header.js
+++ Front End/Bloco 12 - Ciclo de Vida de Componentes e React Router/React Router/my-pokedex/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="text-center p-2 border-b border-gray-300 bg-white w-screen">
-      <h1 className="text-3xl">Pokedex</h1>
+      <Link to="/about" className="text-3xl font-worksans font-medium">Pokedex</Link>
     </header>
   );
 }

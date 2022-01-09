f4b9925dc44b4589a88d7467e18472996ecabd7a
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-09 19:59:51 -0300

    Implemented tests to mock a new default implementation

--- Front End/Bloco 14 - Testes automatizados com React Testing Library/RTL - Mocks e Inputs/tests/service.test.js
+++ Front End/Bloco 14 - Testes automatizados com React Testing Library/RTL - Mocks e Inputs/tests/service.test.js
@@ -21,12 +21,30 @@ describe("generateRandomNumber", () => {
   });
   it("should obey the mocked implementation once", () => {
     // Mock the implementation of the function to be a division
-    generateRandomNumber.mockImplementationOnce((num, num2) => {
-      return num / num2;
-    });
+    generateRandomNumber.mockImplementationOnce((num, num2) => num / num2);
     const result = generateRandomNumber(10, 2);
     expect(result).toBe(5);
     expect(generateRandomNumber).toHaveBeenCalledTimes(12);
     expect(generateRandomNumber()).toBe(10);
+  })
+  it("should be able to receive a new default implementation", () => {
+    // Mock a new default implementation
+    generateRandomNumber.mockImplementation((num, num2, num3) => num * num2 * num3);
+    const result = generateRandomNumber(10, 2, 3);
+    const result2 = generateRandomNumber(10, 2, 4);
+    expect(result).toBe(60);
+    expect(result2).toBe(80);
+    // Validate that the function was called with the new implementation
+    expect(generateRandomNumber).toHaveBeenCalledWith(10, 2, 3);
+    expect(generateRandomNumber).toHaveBeenCalledWith(10, 2, 4);
+    // Create a new mock implementation
+    generateRandomNumber.mockImplementation((num) => num * 2);
+    const result3 = generateRandomNumber(10);
+    const result4 = generateRandomNumber(20);
+    expect(result3).toBe(20);
+    expect(result4).toBe(40);
+    // Validate that the function was called with the new implementation
+    expect(generateRandomNumber).toHaveBeenCalledWith(10);
+    expect(generateRandomNumber).toHaveBeenCalledWith(20);
   })
 });

fa4bba5d10d3ee58661d56efd5d51b69d7c6034d
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-14 10:09:07 -0300

    Completed exercise 1

--- Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React/exercise-react-with-redux-intro/src/TrafficSignal.jsx
+++ Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React/exercise-react-with-redux-intro/src/TrafficSignal.jsx
@@ -1,38 +1,48 @@
-import React from 'react';
-import redSignal from './images/redSignal.jpeg';
-import greenSignal from './images/greenSignal.jpeg';
-import yellowSignal from './images/yellowSignal.jpeg';
+import React from "react";
+import redSignal from "./images/redSignal.jpeg";
+import greenSignal from "./images/greenSignal.jpeg";
+import yellowSignal from "./images/yellowSignal.jpeg";
+import { connect } from "react-redux";
+import { changeSignal } from "./redux/actionCreators.js";
 
 const renderSignal = (signalColor) => {
-  if (signalColor === 'red') {
-    return redSignal;
+  switch (signalColor) {
+    case "red":
+      return redSignal;
+    case "green":
+      return greenSignal;
+    case "yellow":
+      return yellowSignal;
+    default:
+      return null;
   }
-  if (signalColor === 'green') {
-    return greenSignal;
-  }
-  if (signalColor === 'yellow') {
-    return yellowSignal;
-  }
-  return null;
 };
 
-export default function TrafficSignal({ signalColor, changeSignal }) {
+function TrafficSignal({ signalColor, changeSignal }) {
   return (
     <div>
       <div className="button-container">
-        <button type="button">Red</button>
-        <button type="button">Yellow</button>
-        <button type="button">Green</button>
+        <button type="button" onClick={() => changeSignal("red")}>
+          Red
+        </button>
+        <button type="button" onClick={() => changeSignal("yellow")}>
+          Yellow
+        </button>
+        <button type="button" onClick={() => changeSignal("green")}>
+          Green
+        </button>
       </div>
       <img className="signal" src={renderSignal(signalColor)} alt="" />
     </div>
   );
 }
 
-const mapStateToProps = () => ({
-  signalColor: /* Coloque seu código aqui... */
+const mapStateToProps = (state) => ({
+  signalColor: state.signal.color,
 });
 
-const mapDispatchToProps = () => ({
-  changeSignal: /* Coloque seu código aqui... */
+const mapDispatchToProps = (dispatch) => ({
+  changeSignal: (color) => dispatch(changeSignal(color)),
 });
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrafficSignal);

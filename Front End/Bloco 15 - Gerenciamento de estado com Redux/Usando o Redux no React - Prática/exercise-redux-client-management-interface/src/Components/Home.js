bef318b993eefc1f9d13f99b0409afaa7ce89186
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-15 17:25:33 -0300

    Fix typo

--- Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React - Prática/exercise-redux-client-management-interface/src/Components/Home.js
+++ Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React - Prática/exercise-redux-client-management-interface/src/Components/Home.js
@@ -3,7 +3,7 @@ import reduxLogo from "../assets/reduxLogo.png";
 export default function Home() {
   return (
     <div className="grow flex flex-col items-center justify-center">
-      <h1 className="text-3xl text-center">Redux Client Managament Interface</h1>
+      <h1 className="text-3xl text-center">Redux Client Management Interface</h1>
       <img src={reduxLogo} alt="Redux Logo" />
     </div>
   );

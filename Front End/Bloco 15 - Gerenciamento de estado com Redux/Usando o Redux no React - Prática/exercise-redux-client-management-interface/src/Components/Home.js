bd166ffd20a1db116f5c3f867703f989da13ae1a
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-15 17:25:10 -0300

    Styled Home page

--- Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React - Prática/exercise-redux-client-management-interface/src/Components/Home.js
+++ Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React - Prática/exercise-redux-client-management-interface/src/Components/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React from "react";
+import reduxLogo from "../assets/reduxLogo.png";
 export default function Home() {
   return (
-    <div className='grow flex flex-col items-center justify-center'>
-      Home
+    <div className="grow flex flex-col items-center justify-center">
+      <h1 className="text-3xl text-center">Redux Client Managament Interface</h1>
+      <img src={reduxLogo} alt="Redux Logo" />
     </div>
-  )
+  );
 }

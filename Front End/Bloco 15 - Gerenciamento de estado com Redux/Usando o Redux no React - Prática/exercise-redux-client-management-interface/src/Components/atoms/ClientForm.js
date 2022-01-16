60e4a7cafea56390320f48e4087b341a5ab23b5f
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-15 21:39:32 -0300

    Added constraints to handle emailInput

--- Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React - Prática/exercise-redux-client-management-interface/src/Components/atoms/ClientForm.js
+++ Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React - Prática/exercise-redux-client-management-interface/src/Components/atoms/ClientForm.js
@@ -5,7 +5,7 @@ import { submitClient } from "../../redux/actions/submitClient";
 import store from "../../redux/store";
 import { Input } from "./LoginForm";
 
-const validForm = ({email, name, age}) =>
+const validForm = ({ email, name, age }) =>
   email.length > 12 && name.length > 2 && age > 10;
 
 export default function ClientForm() {
@@ -17,10 +17,13 @@ export default function ClientForm() {
   const [age, setAge] = useState("");
   const handleAge = (ageInput) =>
     /^([1-9][0-9]{0,1})$|^$/.test(ageInput) && setAge(ageInput);
-  const handleName = (nameInput) => setName(nameInput.replace(/[^a-zA-Z ]/g, ""));
+  const handleName = (nameInput) =>
+    nameInput < 20 && setName(nameInput.replace(/[^a-zA-Z ]/g, ""));
+  const handleEmail = (emailInput) =>
+    emailInput.length < 30 && setEmail(emailInput);
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = {email, name, age: Number(age)};
+    const formData = { email, name, age: Number(age) };
     if (validForm(formData)) {
       if (clients.find((client) => client.email === email)) {
         alert("Client already exists");
@@ -37,7 +40,7 @@ export default function ClientForm() {
     >
       <Input type="name" onChange={handleName} value={name} />
       <Input type="age" onChange={handleAge} value={age} />
-      <Input type="email" onChange={setEmail} value={email} />
+      <Input type="email" onChange={handleEmail} value={email} />
       <button type="submit" className="ml-auto -mt-1 mr-0.5 block text-sky-500">
         Register
       </button>

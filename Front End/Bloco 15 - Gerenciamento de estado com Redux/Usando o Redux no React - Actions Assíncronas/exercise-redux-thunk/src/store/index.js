98c2f002759c271c1dd64572768e6849dee5e5a0
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-17 01:45:17 -0300

    Applied middleware

--- Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React - Actions Assíncronas/exercise-redux-thunk/src/store/index.js
+++ Front End/Bloco 15 - Gerenciamento de estado com Redux/Usando o Redux no React - Actions Assíncronas/exercise-redux-thunk/src/store/index.js
@@ -1,11 +1,13 @@
 // Importe o método applyMiddleware
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 // Importe o redux-thunk
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 // Aplique o middleware
 const store = createStore(
   rootReducer,
+  applyMiddleware(thunk)
 );
 
 export default store;

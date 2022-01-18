893f90f48cb64795cdfcb763e02f31a3ebcb08d8
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-18 18:39:32 -0300

    Implemented useState to update input value and useEffect to update localStorage

--- Front End/Bloco 17 - Context API e React Hooks/React Hooks -  useEffect e Hooks customizados/exercise-hooks-useEffect-customHooks-exercise-two.two/src/App.js
+++ Front End/Bloco 17 - Context API e React Hooks/React Hooks -  useEffect e Hooks customizados/exercise-hooks-useEffect-customHooks-exercise-two.two/src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-function Greeting({ initialName = '' }) {
-  // 🐨 inicialize o estado com o valor que vem do localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName);
+function Greeting({ initialName = "" }) {
+  const [name, setName] = React.useState(
+    window.localStorage.getItem("name") || initialName
+  );
 
-  // 🐨 Utilize o hook useEffect para atualizar a
-  // propriedade `name` no localStorage quando o estado for alterado
-  // 💰 window.localStorage.setItem('name', name)
-
+  useEffect(
+    () => {
+      window.localStorage.setItem("name", name);
+    },
+    [name]
+  );
   function handleChange(event) {
     setName(event.target.value);
   }
@@ -18,7 +20,7 @@ function Greeting({ initialName = '' }) {
         <label htmlFor="name">Name: </label>
         <input onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {name ? <strong>Hello {name}</strong> : "Please type your name"}
     </div>
   );
 }

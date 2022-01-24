38cb47d6760accdfefdc172bd1f8c71be6c633e9
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-24 14:43:34 -0300

    Added user input as source for imc calc

--- Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/NodeJS - Um motor JavaScript/imc.js
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/NodeJS - Um motor JavaScript/imc.js
@@ -1,3 +1,6 @@
-const getIMC = (height, weight) =>  weight / (height * height);
-
-console.log(getIMC(1.80, 80));
+const readline = require('readline-sync');
+const calcIMC = (height, weight) =>  weight / (height * height);
+const height = readline.question('What\'s your height in meters? ');
+const weight = readline.question('What\'s your weight in kilograms? ');
+const response = `Your IMC is ${calcIMC(height, weight).toFixed(2)}`;
+console.log(response);

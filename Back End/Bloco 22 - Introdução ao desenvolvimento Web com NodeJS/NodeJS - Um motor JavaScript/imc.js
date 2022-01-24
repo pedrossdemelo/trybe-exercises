9e6900db8f2b4756efde284ff0b5ccef8739c3a5
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-24 14:44:15 -0300

    Limit input to floats only

--- Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/NodeJS - Um motor JavaScript/imc.js
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/NodeJS - Um motor JavaScript/imc.js
@@ -1,6 +1,6 @@
 const readline = require('readline-sync');
 const calcIMC = (height, weight) =>  weight / (height * height);
-const height = readline.question('What\'s your height in meters? ');
-const weight = readline.question('What\'s your weight in kilograms? ');
+const height = readline.questionFloat('What\'s your height in meters? ');
+const weight = readline.questionFloat('What\'s your weight in kilograms? ');
 const response = `Your IMC is ${calcIMC(height, weight).toFixed(2)}`;
 console.log(response);

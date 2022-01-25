dff673fafa7520575b92d14b6bc45c533d8f4064
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-24 22:49:03 -0300

    Create function to write multiple files concurrently using Promise.all

--- Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Node.js - Fluxo Assíncrono/readfile.js
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Node.js - Fluxo Assíncrono/readfile.js
@@ -1,69 +1,87 @@
-// Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome . Por exemplo: 1 - Homer Simpson .
-// Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
-// Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
-// Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json , contendo as personagens com id de 1 a 4.
-// Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz .
-// Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json .
+const fs = require("fs").promises;
 
-const fs = require('fs').promises;
-
-const getHomer = () => fs.readFile('simpsons.json', 'utf-8').then((data) => {
-  const simpsons = JSON.parse(data);
-  const homer = simpsons.find((person) => person.id == 1);
-  console.log(homer.name);
-});
-
-const getSimpson = (id) => new Promise((resolve, reject) => {
-  fs.readFile('simpsons.json', 'utf-8').then((data) => {
+const getHomer = () =>
+  fs.readFile("simpsons.json", "utf-8").then((data) => {
     const simpsons = JSON.parse(data);
-    const simpson = simpsons.find((person) => person.id == id);
-    if (simpson) resolve(simpson);
-    else reject(`Id ${id} não encontrado`);
+    const homer = simpsons.find((person) => person.id == 1);
+    console.log(homer.name);
   });
-})
+
+const getSimpson = (id) =>
+  new Promise((resolve, reject) => {
+    fs.readFile("simpsons.json", "utf-8").then((data) => {
+      const simpsons = JSON.parse(data);
+      const simpson = simpsons.find((person) => person.id == id);
+      if (simpson) resolve(simpson);
+      else reject(`Id ${id} não encontrado`);
+    });
+  });
 
 function removeSimpson(...ids) {
-  fs.readFile('simpsons.json', 'utf-8').then((data) => {
+  fs.readFile("simpsons.json", "utf-8").then((data) => {
     const simpsons = JSON.parse(data);
-    const newSimpsons = simpsons.filter((person) => ids.every((id) => person.id != id));
-    fs.writeFile('simpsons.json', JSON.stringify(newSimpsons)).then(() => {
-      console.log('Simpson removido');
+    const newSimpsons = simpsons.filter((person) =>
+      ids.every((id) => person.id != id)
+    );
+    fs.writeFile("simpsons.json", JSON.stringify(newSimpsons)).then(() => {
+      console.log("Simpson removido");
     });
   });
 }
 
 function getSimpsonFamily() {
-  fs.readFile('simpsons.json', 'utf-8').then((data) => {
+  fs.readFile("simpsons.json", "utf-8").then((data) => {
     const simpsons = JSON.parse(data);
     const simpsonFamily = simpsons.slice(0, 4);
-    fs.writeFile('simpsonFamily.json', JSON.stringify(simpsonFamily)).then(() => {
-      console.log('Simpson Family criado');
-    });
+    fs.writeFile("simpsonFamily.json", JSON.stringify(simpsonFamily)).then(
+      () => {
+        console.log("Simpson Family criado");
+      }
+    );
   });
 }
 
 function addNelsonMuntz() {
-  fs.readFile('simpsonFamily.json', 'utf-8').then((data) => {
+  fs.readFile("simpsonFamily.json", "utf-8").then((data) => {
     const simpsons = JSON.parse(data);
     simpsons.push({
       id: 5,
-      name: 'Nelson Muntz',
+      name: "Nelson Muntz",
     });
-    fs.writeFile('simpsonFamily.json', JSON.stringify(simpsons)).then(() => {
-      console.log('Nelson Muntz adicionado');
+    fs.writeFile("simpsonFamily.json", JSON.stringify(simpsons)).then(() => {
+      console.log("Nelson Muntz adicionado");
     });
   });
 }
 
 function replaceNelsonForMaggie() {
-  fs.readFile('simpsonFamily.json', 'utf-8').then((data) => {
+  fs.readFile("simpsonFamily.json", "utf-8").then((data) => {
     const simpsons = JSON.parse(data);
     const newSimpsons = simpsons.map((person) => {
-      if (person.id == 5) person.name = 'Maggie Simpson'; 
+      if (person.id == 5) person.name = "Maggie Simpson";
       return person;
     });
-    fs.writeFile('simpsonFamily.json', JSON.stringify(newSimpsons)).then(() => {
-      console.log('Nelson Muntz substituido por Maggie Simpson');
+    fs.writeFile("simpsonFamily.json", JSON.stringify(newSimpsons)).then(() => {
+      console.log("Nelson Muntz substituido por Maggie Simpson");
     });
   });
 }
+
+// Crie uma função que lê e escreve vários arquivos ao mesmo tempo.
+
+//     Utilize o Promise.all para manipular vários arquivos ao mesmo tempo.
+//     Dado o seguinte array de strings: ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'] Faça com que sua função crie um arquivo contendo cada string, sendo o nome de cada arquivo igual a file<index + 1>.txt . Por exemplo, para a string "Finalmente", o nome do arquivo é file1.txt .
+//     Programe sua função para que ela faça a leitura de todos os arquivos criados no item anterior, armazene essa informação e escreva em um arquivo chamado fileAll.txt .
+
+const stringArray = ["Finalmente", "estou", "usando", "Promise.all", "!!!"];
+
+function writeTofiles(strings) {
+  const promises = strings.map((string, index) => {
+    return fs.writeFile(`file${index + 1}.txt`, string);
+  });
+  return Promise.all(promises).then(() => {
+    console.log("Arquivos criados");
+  });
+}
+
+writeTofiles(stringArray);

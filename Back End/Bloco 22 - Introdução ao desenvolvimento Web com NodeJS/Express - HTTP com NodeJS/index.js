1c7bbdd5b03efa45085c03d9838fbdf1518bd9e9
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-25 20:28:53 -0300

    Add /greetings post route

--- Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Express - HTTP com NodeJS/index.js
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Express - HTTP com NodeJS/index.js
@@ -4,11 +4,17 @@ app.use(express.json());
 
 app.get('/ping', (_, res) => {
   res.json({message: 'pong'});
-})
+});
 
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.json({ message: `Hello ${name}` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (age > 17) return res.status(200).json({ message: `Hello ${name}` });
+  return res.status(401).json({ message: "Unauthorized" });
 });
 
 app.listen(3001, _ => console.log('Server running on port 3001'));

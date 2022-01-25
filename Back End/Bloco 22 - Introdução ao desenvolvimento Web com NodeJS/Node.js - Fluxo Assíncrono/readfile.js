5246a3fde331f95719f80e3b602556cefc473f34
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-24 21:46:58 -0300

    Get homer from reading simpsons.json file

---
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Node.js - Fluxo Assíncrono/readfile.js
@@ -0,0 +1,16 @@
+// Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome . Por exemplo: 1 - Homer Simpson .
+// Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
+// Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
+// Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json , contendo as personagens com id de 1 a 4.
+// Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz .
+// Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json .
+
+const fs = require('fs').promises;
+
+const getHomer = () => fs.readFile('simpsons.json', 'utf-8').then((data) => {
+  const simpsons = JSON.parse(data);
+  const homer = simpsons.find((person) => person.id == 1);
+  console.log(homer.name);
+});
+
+getHomer();

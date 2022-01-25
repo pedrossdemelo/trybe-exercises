c43e96427b12626c552ae92934d6af0c206d2e1d
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-25 14:16:57 -0300

    Implement tests for invalid param exception

--- Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Mocha, Chai e Sinon - Testes de Back-end com NodeJS/tests/test.js
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Mocha, Chai e Sinon - Testes de Back-end com NodeJS/tests/test.js
@@ -10,6 +10,27 @@
 // testes que serão feitos utilizando its ; Crie as asserções validando se os
 // retornos de cada cenário tem o tipo e o valor esperado.
 
+// Exercício 3 Adicione à função um tratamento para casos em que o parâmetro informado não seja do tipo Number .
+
+// Adicione o cenário em seu arquivo de testes, passando um valor de tipo diferente a Number para a função;
+// Adicione uma asserção para esperar que o valor retornado para esse caso seja igual uma string "o valor deve ser um número";
+// Implemente em sua função tal validação para que o teste passe.
+
+// Exercício 4 Crie testes para uma função que escreverá um conteúdo em um arquivo específico.
+
+// Essa função deverá receber dois parâmetros: o nome do arquivo e o conteúdo desse arquivo.
+// Após concluir a escrita do arquivo ela deverá retornar um ok .
+
+// Descreva todos os cenários de teste utilizando describes ;
+// Descreva todos os testes que serão feitos utilizando its ;
+// Crie as asserções validando se o retorno da função possui o valor e tipo esperado.
+
+// Exercício 5 Implemente a função descrita no exercício 4.
+
+// Crie a função descrita no exercício 4 utilizando o módulo fs do node.
+// Adapte os testes adicionando stub ao módulo utilizado do fs , isolando assim o teste.
+// Garanta que todos os testes escritos no exercício 4 irão passar com sucesso.
+
 const { expect } = require('chai');
 const sinon = require('sinon');
 const { evalNum } = require('../utils/index.js');
@@ -29,4 +50,9 @@ describe('Testando a função evalNum', () => {
     const result = evalNum(0);
     expect(result).to.be.equal('neutro');
   });
+
+  it('Deve retornar "o valor deve ser um número" quando o parametro fornecido for diferente de um numero', () => {
+    const result = evalNum('a');
+    expect(result).to.be.equal('o valor deve ser um número');
+  })
 });
--- Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Mocha, Chai e Sinon - Testes de Back-end com NodeJS/utils/index.js
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Mocha, Chai e Sinon - Testes de Back-end com NodeJS/utils/index.js
@@ -1,11 +1,8 @@
 module.exports = {
   evalNum: (num) => {
-    if (num > 0) {
-      return 'positivo';
-    } else if (num < 0) {
-      return 'negativo';
-    } else {
-      return 'neutro';
-    }
-  }
+    if (typeof num !== "number") return "o valor deve ser um número";
+    if (num > 0) return "positivo";
+    if (num < 0) return "negativo";
+    return "neutro";
+  },
 };

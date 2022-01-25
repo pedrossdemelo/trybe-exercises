d1ada5f7017b0e34c50b02266fa9f9c11353bbd7
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-25 20:26:27 -0300

    Add /hello post route

--- Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Express - HTTP com NodeJS/index.js
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Express - HTTP com NodeJS/index.js
@@ -6,4 +6,9 @@ app.get('/ping', (_, res) => {
   res.json({message: 'pong'});
 })
 
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `Hello ${name}` });
+});
+
 app.listen(3001, _ => console.log('Server running on port 3001'));

2060a47d1fd1211dd85d1894fd7b7d9239270766
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-24 23:00:32 -0300

    Install read-line sync

---
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Node.js - Fluxo Assíncrono/readfileCLI.js
@@ -0,0 +1,8 @@
+// Para os exercícios abaixo, faremos uso de um módulo chamado readline , principalmente de seu método readline.question() . Ele provê uma interface de leitura de dados inserida no terminal. Para mais informações, consulte a documentação .
+
+//     Crie um script que mostre na tela o conteúdo de um arquivo escolhido pela pessoa usuária:
+//         Pergunte à pessoa usuária qual arquivo ela deseja ler.
+//         Leia o arquivo indicado.
+//         Caso o arquivo não exista, exiba na tela "Arquivo inexistente" e encerre a execução do script.
+//         Caso o arquivo exista, escreva seu conteúdo na tela.
+

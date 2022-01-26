2c8290e573faf71f2b9dcd58793dbb4df0025fa7
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-26 12:57:24 -0300

    Implement teams route

--- Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Express - Middlewares/index.js
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Express - Middlewares/index.js
@@ -4,11 +4,13 @@ const rescue = require("express-rescue");
 const userRouter = require("./routes/user");
 const btcRouter = require("./routes/btc");
 const postsRouter = require("./routes/posts");
+const teamsRouter = require("./routes/teams");
 const errorMiddleware = require("./errorMiddleware");
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/btc", btcRouter);
 app.use("/posts", postsRouter);
+app.use("/teams", teamsRouter);
 app.use(errorMiddleware);
 app.use("*", (_, res) =>
   res.status(404).json({ code: "Not found", message: "Route not found" })

728fd59493cfb894d851139d3b08437d3fed568a
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-26 12:14:50 -0300

    Implement posts route and default route

--- Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Express - Middlewares/index.js
+++ Back End/Bloco 22 - Introdução ao desenvolvimento Web com NodeJS/Express - Middlewares/index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const rescue = require('express-rescue');
-const userRouter = require('./routes/user');
-const btcRouter = require('./routes/btc');
-const errorMiddleware = require('./errorMiddleware');
+const rescue = require("express-rescue");
+const userRouter = require("./routes/user");
+const btcRouter = require("./routes/btc");
+const postsRouter = require("./routes/posts");
+const errorMiddleware = require("./errorMiddleware");
 app.use(express.json());
-app.use('/user', userRouter);
-app.use('/btc', btcRouter);
-app.use(errorMiddleware)
-app.listen(3001, () => console.log('Server running on port 3001'));
+app.use("/user", userRouter);
+app.use("/btc", btcRouter);
+app.use("/posts", postsRouter);
+app.use(errorMiddleware);
+app.use("*", (_, res) =>
+  res.status(404).json({ code: "Not found", message: "Route not found" })
+);
+app.listen(3001, () => console.log("Server running on port 3001"));

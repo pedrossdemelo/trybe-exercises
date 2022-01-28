80081a0a0024ae27539e2d54c097ee023c100a8b
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-28 20:28:26 -0300

    Add GET /patients endpoint with all plans listed

--- Back End/Bloco 24 - NodeJS - ORM e Autenticação/ORM - Associations/exercise-sequelize-associations/index.js
+++ Back End/Bloco 24 - NodeJS - ORM e Autenticação/ORM - Associations/exercise-sequelize-associations/index.js
@@ -1,11 +1,21 @@
 const bodyParser = require('body-parser');
-
+const { Patient, Plan, Surgery, PatientSurgeries } = require('./models');
 const express = require('express');
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/patients', async (_req, res) => {
+  try {
+    const patients = await Patient.findAll({
+      include: [{ model: Plan, as: 'plan' }],
+    });
+    res.status(200).json(patients);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 const PORT = 3000;
 
 app.listen(PORT, () => {

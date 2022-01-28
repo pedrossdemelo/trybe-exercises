71df92be5cae1f594b07ed5e2060895476f7f063
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-28 20:46:22 -0300

    Add GET /patients endpoint with query boolean to show all surgeries

--- Back End/Bloco 24 - NodeJS - ORM e Autenticação/ORM - Associations/exercise-sequelize-associations/index.js
+++ Back End/Bloco 24 - NodeJS - ORM e Autenticação/ORM - Associations/exercise-sequelize-associations/index.js
@@ -6,12 +6,44 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/patients', async (_req, res) => {
+app.get('/patients', async (req, res) => {
+  const { includePlan, includeSurgeries } = req.query;
+  if (includePlan) {
+    try {
+      const patients = await Patient.findAll({
+        include: [
+          {
+            model: Plan,
+            as: 'plan',
+          },
+        ],
+      });
+      res.json(patients);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  if (includeSurgeries) {
+    try {
+      const patients = await Patient.findAll({
+        include: [
+          {
+            model: Surgery,
+            as: 'surgeries',
+            through: {
+              attributes: []
+            }
+          },
+        ],
+      });
+      res.json(patients);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
   try {
-    const patients = await Patient.findAll({
-      include: [{ model: Plan, as: 'plan' }],
-    });
-    res.status(200).json(patients);
+    const patients = await Patient.findAll();
+    res.json(patients);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }

4faef042161ee7615d5effda24d33abd4b8a3044
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-27 23:42:50 -0300

    Implemented failing tests for movieService.js

--- Back End/Bloco 23 - NodeJS - Camada de Serviço e Arquitetura Rest e Restful/Arquitetura de Software - Testando as Camadas/services/movieService.js
+++ Back End/Bloco 23 - NodeJS - Camada de Serviço e Arquitetura Rest e Restful/Arquitetura de Software - Testando as Camadas/services/movieService.js
@@ -1,9 +1,9 @@
-const MoviesModel = require('../models/movieModel');
+const MoviesModel = require("../models/movieModel");
 
 const isValid = (title, directedBy, releaseYear) => {
-  if (!title || typeof title !== 'string') return false;
-  if (!releaseYear || typeof releaseYear !== 'number') return false;
-  if (!directedBy || typeof directedBy !== 'string') return false;
+  if (!title || typeof title !== "string") return false;
+  if (!releaseYear || typeof releaseYear !== "number") return false;
+  if (!directedBy || typeof directedBy !== "string") return false;
 
   return true;
 };
@@ -13,14 +13,15 @@ const create = async ({ title, directedBy, releaseYear }) => {
 
   if (!isMovieValid) return false;
 
-  const { id } = await MoviesModel
-    .create({ title, directedBy, releaseYear });
+  const { id } = await MoviesModel.create({ title, directedBy, releaseYear });
 
   return {
     id,
   };
 };
 
+const getById = async (id) => {};
 module.exports = {
   create,
+  getById
 };

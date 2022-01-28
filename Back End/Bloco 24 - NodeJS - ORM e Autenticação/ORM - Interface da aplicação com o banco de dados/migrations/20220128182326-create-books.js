3e0a4bd9907b713998a65b9f81804a588bebb5e4
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-28 15:37:29 -0300

    Setup first migration

---
+++ Back End/Bloco 24 - NodeJS - ORM e Autenticação/ORM - Interface da aplicação com o banco de dados/migrations/20220128182326-create-books.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('books');
+  }
+};

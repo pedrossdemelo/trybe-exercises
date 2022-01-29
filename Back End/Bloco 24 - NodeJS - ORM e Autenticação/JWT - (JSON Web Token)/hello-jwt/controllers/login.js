69ec469797a79f50c971ba2826bc659282264a1b
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-29 03:15:37 -0300

    Validate username and password using Joi

--- Back End/Bloco 24 - NodeJS - ORM e Autenticação/JWT - (JSON Web Token)/hello-jwt/controllers/login.js
+++ Back End/Bloco 24 - NodeJS - ORM e Autenticação/JWT - (JSON Web Token)/hello-jwt/controllers/login.js
@@ -1,15 +1,21 @@
 require('dotenv').config();
+const Joi = require('joi');
 const jwt = require('jsonwebtoken');
 const userModel = require('../models/userModel');
 
 module.exports = async (req, res, next) => {
   const { username, password } = req.body;
-  if (!username || !password) {
-    return next({ statusCode: 400, message: 'Missing username or password' });
-  }
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(5).required(),
+    password: Joi.string().min(5).required(),
+  });
+  const { error } = Joi.validate({ username, password }, schema);
+  if (error) return next(error);
   const user = await userModel.getByUsername(username);
   if (!user) return next({ statusCode: 404, message: 'User not found' });
-  if (user.password !== password) { return next({ statusCode: 401, message: 'Wrong password' }); }
+  if (user.password !== password) {
+    return next({ statusCode: 401, message: 'Wrong password' });
+  }
   const JWT = jwt.sign(
     { username, admin: username === 'admin' },
     process.env.JWT_SECRET,

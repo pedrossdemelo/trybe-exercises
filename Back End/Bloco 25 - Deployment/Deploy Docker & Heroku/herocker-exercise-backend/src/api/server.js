4862a1b6879ffb3ccef49f441a24f0de7e4bed07
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-29 20:30:10 -0300

    Setup GET /users endpoint

--- Back End/Bloco 25 - Deployment/Deploy Docker & Heroku/herocker-exercise-backend/src/api/server.js
+++ Back End/Bloco 25 - Deployment/Deploy Docker & Heroku/herocker-exercise-backend/src/api/server.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const app = require('./app');
+const UserController = require('../controllers/user');
+
+app.get('/users', UserController);
 
 const PORT = process.env.PORT || 3000;
 

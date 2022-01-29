9e25c7f1cf8aeb2b7b445fcd6b2c9c154e604c41
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-29 15:18:08 -0300

    Setup env variable to deploy a different response

--- Back End/Bloco 25 - Deployment/Infraestrutura - Deploy com Heroku/heroku-api/index.js
+++ Back End/Bloco 25 - Deployment/Infraestrutura - Deploy com Heroku/heroku-api/index.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const MESSAGE = process.env.MESSAGE || 'Hello World';
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Está vivo!");
+  res.send(MESSAGE);
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

f43d5a5c547a62469f0f6b3dbcc26b48a8a51788
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-30 04:43:08 -0300

    Implement capacity unit conversion function

---
+++ Back End/Bloco 26 - Typescript/Introdução ao TypeScript/src/capacity.ts
@@ -0,0 +1,20 @@
+type CapacityUnit = "kl" | "hl" | "dal" | "l" | "dl" | "cl" | "ml";
+export const convert = (
+  capacity: number,
+  from: CapacityUnit,
+  to: CapacityUnit
+): number => {
+  if (from === to) return capacity;
+  const factors = {
+    kl: 1000,
+    hl: 100,
+    dal: 10,
+    l: 1,
+    dl: 0.1,
+    cl: 0.01,
+    ml: 0.001,
+  };
+  const fromFactor = factors[from];
+  const toFactor = factors[to];
+  return capacity * fromFactor / toFactor;
+}

47a1cdf4b8c708f769698bc145425751d3a87f5b
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-30 04:38:24 -0300

    Implement mass unit conversion function

---
+++ Back End/Bloco 26 - Typescript/Introdução ao TypeScript/src/mass.ts
@@ -0,0 +1,16 @@
+type MassUnit = "kg" | "hg" | "dag" | "g" | "dg" | "cg" | "mg";
+export const convert = (mass: number, from: MassUnit, to: MassUnit): number => {
+  if (from === to) return mass;
+  const factors = {
+    kg: 1000,
+    hg: 100,
+    dag: 10,
+    g: 1,
+    dg: 0.1,
+    cg: 0.01,
+    mg: 0.001,
+  };
+  const fromFactor = factors[from];
+  const toFactor = factors[to];
+  return mass * fromFactor / toFactor;
+}

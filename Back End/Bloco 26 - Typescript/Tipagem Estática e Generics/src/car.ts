03781a0e55399399b0a08621e9dc527aa1b9047a
Pedro Sousa <pedrossm2000@gmail.com>
2022-01-30 18:21:35 -0300

    Create Uber Ride script

--- Back End/Bloco 26 - Typescript/Tipagem Estática e Generics/src/car.ts
+++ Back End/Bloco 26 - Typescript/Tipagem Estática e Generics/src/car.ts
@@ -1,18 +1,18 @@
-enum Colors {
+export enum Colors {
   Black = "black",
   White = "white",
   Red = "red",
   Silver = "silver",
 }
 
-enum Doors {
+export enum Doors {
   FrontLeft = "front left",
   FrontRight = "front right",
   BackLeft = "back left",
   BackRight = "back right",
 }
 
-enum Directions {
+export enum Directions {
   Forward = "forwards",
   Backward = "backwards",
   Left = "left",
@@ -23,36 +23,46 @@ export class Car {
   brand: string;
   color: Colors;
   doors: number;
+
   constructor(brand: string, color: Colors, doors: number) {
     this.brand = brand;
     this.color = color;
     this.doors = doors;
   }
+
   honk(): void {
-    console.log('Beep!');
+    console.log("Beep!");
   }
+
   openTheDoor(door: Doors): void {
     console.log(`Opening the ${door} door`);
   }
+
   closeTheDoor(door: Doors): void {
     console.log(`Closing the ${door} door`);
   }
+
   turnOn(): void {
-    console.log('The car is on');
+    console.log(`The ${this.color} ${this.brand} is on`);
   }
+
   turnOff(): void {
-    console.log('The car is off');
+    console.log(`The ${this.color} ${this.brand} is off`);
   }
+
   speedUp(): void {
-    console.log('The car is speeding up');
+    console.log(`The ${this.color} ${this.brand} is speeding up`);
   }
+
   speedDown(): void {
-    console.log('The car is slowing down');
+    console.log(`The ${this.color} ${this.brand} is slowing down`);
   }
+
   stop(): void {
-    console.log('The car is stopped');
+    console.log(`The ${this.color} ${this.brand} stopped`);
   }
+
   turn(direction: Directions): void {
-    console.log(`The car is turning ${direction}`);
+    console.log(`The ${this.color} ${this.brand} is turning ${direction}`);
   }
 }

6d02ae910b0074f08f2a3ea77b0df28b911797d8
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-02 01:36:26 -0300

    Add CRUD to /plants

--- Back End/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/SOLID - Introdução e Princípios S, O e D/PlantRouter.ts
+++ Back End/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/SOLID - Introdução e Princípios S, O e D/PlantRouter.ts
@@ -13,5 +13,26 @@ router.get("/:id", async (req, res) => {
   if (!plant) return res.status(404).json({ error: "Plant not found" });
   res.json(plant);
 });
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const plant = await db.removePlantById(id);
+  if (!plant) return res.status(404).json({ error: "Plant not found" });
+  res.status(204).json(plant);
+})
+router.post("/:id", async (req, res) => {
+  const { id } = req.params;
+  const plant = await db.editPlant(id, req.body);
+  if (!plant) return res.status(404).json({ error: "Plant not found" });
+  res.status(201).json(plant);
+})
+router.post("/", async (req, res) => {
+  const plant = await db.savePlant(req.body);
+  res.status(201).json(plant);
+})
+router.get("/sunny/:id", async (req, res) => {
+  const { id } = req.params;
+  const plants = await db.getPlantsThatNeedsSunWithId(id);
+  res.json(plants);
+})
 
 export default router;

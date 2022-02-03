1b631b0dc0706bb6490de153a97687454d3b7430
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-03 02:23:34 -0300

    Query id, name where book _id = 8

--- Back End/Bloco 29 - Introdução ao MongoDB/MongoDB - Introdução/index.js
+++ Back End/Bloco 29 - Introdução ao MongoDB/MongoDB - Introdução/index.js
@@ -13,6 +13,9 @@ const { MongoClient } = require("mongodb");
     console.log("Exercise 1");
     console.log(await bios.findOne({ _id: 8 }));
     
+    console.log("Exercise 2");
+    console.log(await bios.findOne({ _id: 8 }, { _id: 1, name: 1 }));
+
   } catch (error) {
     console.error(`Something went wrong: ${error}`);
   } finally {

eedde0221e461a466d678216b42613d53a75798d
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-03 03:14:17 -0300

    Query all heroes below 180cm

--- Back End/Bloco 29 - Introdução ao MongoDB/Filter Operators/index.js
+++ Back End/Bloco 29 - Introdução ao MongoDB/Filter Operators/index.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+const { MongoClient } = require('mongodb');
 
 (async function main() {
   const client = new MongoClient("mongodb://localhost:27017");
@@ -11,6 +11,13 @@ import { MongoClient } from "mongodb";
 
     console.log("Exercise 1")
     console.log(await superheroes.findOne({}));
+
+    console.log("Exercise 2")
+    console.log(await superheroes.find({
+      "aspects.height": {
+        $lt: 180
+      }
+    }).toArray());
 
   } catch (error) {
     console.log(`Something went wrong: ${error}`);

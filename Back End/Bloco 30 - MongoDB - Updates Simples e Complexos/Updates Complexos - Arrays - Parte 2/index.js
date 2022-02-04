bdaf3792e0dabe054070be031c021870fd7f048f
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-04 02:56:12 -0300

    Add the description field in the movie Home Alone with the following value: 'An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation.'

--- Back End/Bloco 30 - MongoDB - Updates Simples e Complexos/Updates Complexos - Arrays - Parte 2/index.js
+++ Back End/Bloco 30 - MongoDB - Updates Simples e Complexos/Updates Complexos - Arrays - Parte 2/index.js
@@ -173,6 +173,20 @@ const { MongoClient } = require("mongodb");
       )
     );
     console.log("Created a description for the Godzilla movie");
+
+    console.log("Exercise 17");
+    console.log(
+      await movies.updateOne(
+        { title: "Home Alone" },
+        {
+          $set: {
+            description:
+              "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation.",
+          },
+        }
+      )
+    );
+    console.log("Created a description for the Home Alone movie");
   } catch (error) {
     console.error(error);
   } finally {

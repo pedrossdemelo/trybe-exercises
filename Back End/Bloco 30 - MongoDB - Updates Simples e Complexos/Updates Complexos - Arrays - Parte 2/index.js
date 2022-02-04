b181ef6a6e253d79bf544943db75f113a5ce7f13
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-04 01:04:39 -0300

    Setup collection connection

---
+++ Back End/Bloco 30 - MongoDB - Updates Simples e Complexos/Updates Complexos - Arrays - Parte 2/index.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require("mongodb");
+
+(async function main() {
+  const client = new MongoClient("mongodb://localhost:27017/");
+
+  try {
+    await client.connect();
+    const db = client.db("class");
+    const movies = db.collection("movies");
+
+    await db.dropCollection("movies");
+    await movies.insertMany([
+      {
+        title: "Batman",
+        category: ["action", "adventure"],
+        imdbRating: 7.7,
+        budget: 35,
+      },
+      {
+        title: "Godzilla",
+        category: ["action", "adventure", "sci-fi"],
+        imdbRating: 6.6,
+        budget: 10,
+      },
+      {
+        title: "Home Alone",
+        category: ["family", "comedy"],
+        imdbRating: 7.4,
+      },
+    ]);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.close();
+  }
+})();

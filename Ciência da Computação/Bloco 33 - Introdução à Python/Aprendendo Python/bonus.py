40ae572de498ea9ed54457c08b5a83eaee69a9fd
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-04 18:52:08 -0300

    Add fn that creates a square triangle with n size

--- Ciência da Computação/Bloco 33 - Introdução à Python/Aprendendo Python/bonus.py
+++ Ciência da Computação/Bloco 33 - Introdução à Python/Aprendendo Python/bonus.py
@@ -4,3 +4,13 @@ def smallest(*array):
 
 
 print(smallest(1, 2, 3, 4, 5))
+
+
+def create_square_triangle(size):
+    triangle = ""
+    for i in range(1, size + 1):
+        triangle += "*" * i + " " * (size - i) + "\n"
+    return triangle
+
+
+print(create_square_triangle(5))

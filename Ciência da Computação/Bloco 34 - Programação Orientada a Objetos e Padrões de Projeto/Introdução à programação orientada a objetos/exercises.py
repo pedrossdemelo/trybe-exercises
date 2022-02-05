7d49a845ee20c112698ab6670dc6c2f162bb33a5
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-05 13:37:42 -0300

    Add Circle class

--- Ciência da Computação/Bloco 34 - Programação Orientada a Objetos e Padrões de Projeto/Introdução à programação orientada a objetos/exercises.py
+++ Ciência da Computação/Bloco 34 - Programação Orientada a Objetos e Padrões de Projeto/Introdução à programação orientada a objetos/exercises.py
@@ -9,3 +9,14 @@ class Rectangle:
 
     def get_perimeter(self) -> int:
         return (2 * self.height) + (2 * self.width)
+
+
+class Circle:
+    def __init__(self, radius) -> None:
+        self.radius, self.width, self.height = radius, radius * 2, radius * 2
+    
+    def get_area(self) -> int:
+        return 3.14 * (self.radius ** 2)
+    
+    def get_perimeter(self) -> int:
+        return 2 * 3.14 * self.radius

d65859bfb2f29b38a6b635087b064047c32e3680
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-05 12:05:33 -0300

    Test remove_invalid_emails

--- Ciência da Computação/Bloco 33 - Introdução à Python/Testes/test_exercises.py
+++ Ciência da Computação/Bloco 33 - Introdução à Python/Testes/test_exercises.py
@@ -14,3 +14,12 @@ def test_sequence_to_phonenum():
     assert sequence_to_phonenum("") == ""
     with pytest.raises(TypeError, match="missing 1 required positional argument"):
         sequence_to_phonenum()
+
+
+def test_remove_invalid_emails():
+    email_list = ["pedro@test.com", "testinguser@test.com",
+                  "invalidmail", "invalid@mail", "invalid@mailtest.commerce", "", "&&#45@email.com"]
+    valid_mail_list = ["pedro@test.com", "testinguser@test.com"]
+    assert remove_invalid_emails(email_list) == valid_mail_list
+    with pytest.raises(TypeError, match="missing 1 required positional argument"):
+        remove_invalid_emails()

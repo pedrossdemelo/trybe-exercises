bef8b48532641f5b35162b4b341fbbbcc49ca6ae
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-05 12:02:01 -0300

    Add sequence_to_phonenum fn

--- Ciência da Computação/Bloco 33 - Introdução à Python/Testes/exercises.py
+++ Ciência da Computação/Bloco 33 - Introdução à Python/Testes/exercises.py
@@ -10,3 +10,20 @@ def fizzbuzz_seq(end):
         else:
             result.append(i)
     return result
+
+
+def sequence_to_phonenum(sequence):
+    map = {
+        "ABC": 2,
+        "DEF": 3,
+        "GHI": 4,
+        "JKL": 5,
+        "MNO": 6,
+        "PQRS": 7,
+        "TUV": 8,
+        "WXYZ": 9
+    }
+    for key in map:
+        for char in key:
+            sequence = sequence.replace(char, str(map[key]))
+    return sequence

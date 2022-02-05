73de09e1e9614fc9300593dfc11ccfa45a22c729
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-05 00:50:46 -0300

    Added guessing game

--- Ciência da Computação/Bloco 33 - Introdução à Python/Entrada e Saída de Dados/exercises.py
+++ Ciência da Computação/Bloco 33 - Introdução à Python/Entrada e Saída de Dados/exercises.py
@@ -1,7 +1,29 @@
+import random
+
+
 def print_as_sqr_tri(string):
     size = len(string)
     for i in range(size, 0, -1):
         print(string[:i])
 
 
-print_as_sqr_tri("PEDRO")
+# print_as_sqr_tri("PEDRO")
+
+def guessing_game():
+    words = ["CHOSEN", "PYTHON", "CODING", "CRYING",
+             "SLEEPY", "SLEEPS", "SOWING", "SEWING"]
+    chosen_word, tries = random.choice(words), 3
+    while tries > 0:
+        scrambled_word = "".join(random.sample(chosen_word, len(chosen_word)))
+        answer = input(
+            f"What is the word \"{scrambled_word}\"? (You have {tries} tries left) ")
+        if answer == chosen_word:
+            print("You win!")
+            break
+        else:
+            tries -= 1
+            print("Wrong, try again!" if tries >
+                  0 else f"You lose! The word was {chosen_word}")
+
+
+guessing_game()

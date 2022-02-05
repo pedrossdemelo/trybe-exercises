6ca18b9df214f3100c404ffae43e88b28304f687
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-05 13:41:23 -0300

    Add Order and OrderItem classes

--- Ciência da Computação/Bloco 34 - Programação Orientada a Objetos e Padrões de Projeto/Introdução à programação orientada a objetos/exercises.py
+++ Ciência da Computação/Bloco 34 - Programação Orientada a Objetos e Padrões de Projeto/Introdução à programação orientada a objetos/exercises.py
@@ -14,9 +14,36 @@ class Rectangle:
 class Circle:
     def __init__(self, radius) -> None:
         self.radius, self.width, self.height = radius, radius * 2, radius * 2
-    
+
     def get_area(self) -> int:
         return 3.14 * (self.radius ** 2)
-    
+
     def get_perimeter(self) -> int:
         return 2 * 3.14 * self.radius
+
+
+# Exercício 4: Vamos mudar um pouco nosso contexto para um sistema de vendas de uma cafeteria. Como podemos abstrair um pedido composto por vários itens? Qual seu nome, atributos e comportamentos?
+
+class OrderItem:
+    def __init__(self, name, price, quantity) -> None:
+        self.name, self.price, self.quantity = name, price, quantity
+
+    def get_total(self) -> float:
+        return self.price * self.quantity
+
+
+class Order:
+    def __init__(self, items) -> None:
+        self.items = items
+
+    def get_total(self) -> float:
+        total = 0
+        for item in self.items:
+            total += item.get_total()
+        return total
+    
+    def get_item_names(self) -> list:
+        names = []
+        for item in self.items:
+            names.append(item.name)
+        return names

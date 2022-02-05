7776828b05275e2f66a1eaa9e59a8dabe7e17121
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-05 19:25:08 -0300

    Define abstract Shape, Cicle, Square and Rectangle classes

--- Ciência da Computação/Bloco 34 - Programação Orientada a Objetos e Padrões de Projeto/Herança, Composição e Interfaces/exercises.py
+++ Ciência da Computação/Bloco 34 - Programação Orientada a Objetos e Padrões de Projeto/Herança, Composição e Interfaces/exercises.py
@@ -1,3 +1,6 @@
+from abc import abstractmethod, abstractproperty
+
+
 class TV:
     def __init__(self, size):
         self.__size = size
@@ -59,3 +62,54 @@ class Statistics:
             else:
                 mode_dict[item] = 1
         return max(mode_dict, key=mode_dict.get)
+
+
+class Shape:
+    @abstractproperty
+    def area(self):
+        raise NotImplementedError
+    
+    @abstractproperty
+    def perimeter(self):
+        raise NotImplementedError
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def area(self):
+        return self.width * self.height
+
+    @property
+    def perimeter(self):
+        return (self.width + self.height) * 2
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    @property
+    def area(self):
+        return self.side ** 2
+
+    @property
+    def perimeter(self):
+        return self.side * 4
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    @property
+    def area(self):
+        return self.radius ** 2 * 3.14
+
+    @property
+    def perimeter(self):
+        return self.radius * 2 * 3.14
+

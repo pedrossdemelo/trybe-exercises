da64b02625f89e3d04b808eca4bea416551c1e9b
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-06 00:48:09 -0300

    Import broken code

---
+++ Ciência da Computação/Bloco 34 - Programação Orientada a Objetos e Padrões de Projeto/Padrões de projeto/starwars.py
@@ -0,0 +1,26 @@
+class Soldier:
+    def __init__(self, level):
+        self.level = level
+
+    def attack(self):
+        return self.level * 1
+
+
+class Jedi:
+    def __init__(self, level):
+        self.level = level
+
+    def attackWithSaber(self):
+        return self.level * 100
+
+
+class StarWarsGame:
+    def __init__(self, character):
+        self.character = character
+
+    def fight_enemy(self):
+        print(f"You caused {self.character.attack()} of damage to the enemy")
+
+
+StarWarsGame(Soldier(5)).fight_enemy()
+StarWarsGame(Jedi(20)).fight_enemy()

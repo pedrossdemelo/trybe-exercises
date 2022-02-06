61827224825ca1f93b438d8f088e908e4fe4db5f
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-06 14:00:31 -0300

    GET google.com and follow redirects

--- Ciência da Computação/Bloco 35 - Redes e Raspagem de Dados/Arquitetura de redes/curl.sh
+++ Ciência da Computação/Bloco 35 - Redes e Raspagem de Dados/Arquitetura de redes/curl.sh
@@ -1,3 +1,6 @@
 curl -X GET --url https://api.github.com/zen # GET Zen of Github
 curl -X POST https://reqbin.com/echo/post/json -d '{"login":"my_login","password":"my_password"}' # POST JSON to reqbin.com
 curl -X POST https://reqbin.com/echo/post/json -H "Content-Type: application/json" -d '{"productId": 123456, "quantity": 100}' # POST JSON to reqbin.com with headers
+
+curl -X GET https://google.com # GET google.com, response code: 301 (Moved Permanently)
+curl -X GET -L https://google.com # GET google.com follow redirects, response code: 200 (OK)

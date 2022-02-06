d28a9de8e1f0ad7d197c70c7cf1c909a1c5db2da
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-06 18:51:48 -0300

    Block http traffic

--- Ciência da Computação/Bloco 35 - Redes e Raspagem de Dados/Redes de computadores, ferramentas e segurança/exercises.sh
+++ Ciência da Computação/Bloco 35 - Redes e Raspagem de Dados/Redes de computadores, ferramentas e segurança/exercises.sh
@@ -1,2 +1,3 @@
 iptables -A INPUT -p icmp --icmp-type echo-request -j DROP # block all icmp echo requests
 iptables -D INPUT -p icmp --icmp-type echo-request -j DROP # remove the rule
+iptables -A OUTPUT -p tcp --sport 443 -j DROP # block all http traffic

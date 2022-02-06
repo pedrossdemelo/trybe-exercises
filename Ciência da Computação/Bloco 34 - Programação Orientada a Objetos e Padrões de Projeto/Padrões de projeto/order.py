862282f7b7d7a8140522097c847c1df13857bd48
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-06 03:42:59 -0300

    Fix smelly code, fix Data Clumps

--- Ciência da Computação/Bloco 34 - Programação Orientada a Objetos e Padrões de Projeto/Padrões de projeto/order.py
+++ Ciência da Computação/Bloco 34 - Programação Orientada a Objetos e Padrões de Projeto/Padrões de projeto/order.py
@@ -1,18 +1,49 @@
-class Order:
-    def __init__(
-        self,
-        items,
-        credit_card_name,
-        credit_card_number,
-        credit_card_month,
-        credit_card_year,
-        credit_card_security_code,
-    ):
-        self.items = items
-        self.credit_card_name = credit_card_name
-        self.credit_card_number = credit_card_number
-        self.credit_card_month = credit_card_month
-        self.credit_card_year = credit_card_year
-        self.credit_card_security_code = credit_card_security_code
+from datetime import datetime
 
-    # ...
+
+class CreditCard:
+    def __init__(self, name, number, security_code, expiration_date):
+        self.name = name
+        self.number = number
+        self.security_code = security_code
+        self.expiration_date = datetime.strptime(expiration_date, "%m/%y")
+
+    def __str__(self):
+        expiraton = self.expiration_date.strftime("%m/%y")
+        return f"{self.name} - {self.number} - {self.security_code} - {expiraton}"
+
+    def __repr__(self):
+        return str(self)
+
+
+class Order:
+    def __init__(self, items, credit_card):
+        self.items = items
+        self.credit_card = credit_card
+
+    def __str__(self):
+        order_list_formatted = "\n".join([item for item in self.items])
+        return f"{order_list_formatted}\n{self.credit_card}"
+
+    def __repr__(self):
+        return str(self)
+
+
+credit_card = CreditCard("John Doe", "1234567890123456", "123", "12/20")
+items = [
+    "Coffee (hot)",
+    "Cheese",
+    "Milk",
+    "Bread",
+    "Butter",
+]
+order = Order(items, credit_card)
+print(order)
+'''
+Coffee (hot)
+Cheese
+Milk
+Bread
+Butter
+John Doe - 1234567890123456 - 123 - 12/20
+'''

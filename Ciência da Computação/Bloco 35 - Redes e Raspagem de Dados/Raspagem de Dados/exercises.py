68d31c300eb0192949ab9f0d209e30a47022bc23
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-06 21:25:17 -0300

    Print {login} {url} for all elements in github_users

--- Ciência da Computação/Bloco 35 - Redes e Raspagem de Dados/Raspagem de Dados/exercises.py
+++ Ciência da Computação/Bloco 35 - Redes e Raspagem de Dados/Raspagem de Dados/exercises.py
@@ -2,4 +2,11 @@ import requests
 
 
 body = requests.get("https://httpbin.org/encoding/utf8").text
-print(body)
+# print(body)
+
+
+github_json = requests.get("https://api.github.com/users").json()
+github_users = ["{} {}".format(user["login"], user["url"]) for user in github_json]
+for user in github_users:
+    print(user)
+

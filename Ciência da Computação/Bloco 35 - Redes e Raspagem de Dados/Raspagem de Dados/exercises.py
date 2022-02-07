3e7dcb97f413b7a48c2c7af12737b2ce66141c42
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-06 21:37:46 -0300

    Try to pass bot detection while scraping

--- Ciência da Computação/Bloco 35 - Redes e Raspagem de Dados/Raspagem de Dados/exercises.py
+++ Ciência da Computação/Bloco 35 - Redes e Raspagem de Dados/Raspagem de Dados/exercises.py
@@ -7,6 +7,9 @@ body = requests.get("https://httpbin.org/encoding/utf8").text
 
 github_json = requests.get("https://api.github.com/users").json()
 github_users = ["{} {}".format(user["login"], user["url"]) for user in github_json]
-for user in github_users:
-    print(user)
+# for user in github_users:
+#     print(user)
 
+
+test_bot_detection = requests.get("https://scrapethissite.com/pages/advanced/?gotcha=headers").text
+print("bot detected" not in test_bot_detection) # False

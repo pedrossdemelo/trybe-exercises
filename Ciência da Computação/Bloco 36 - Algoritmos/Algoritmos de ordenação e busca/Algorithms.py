2ba2918e3598898bc4f2e5c505601500b6eff962
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-08 17:13:55 -0300

    Print bubble sort algorithm after 2 iterations

--- Ciência da Computação/Bloco 36 - Algoritmos/Algoritmos de ordenação e busca/Algorithms.py
+++ Ciência da Computação/Bloco 36 - Algoritmos/Algoritmos de ordenação e busca/Algorithms.py
@@ -1,5 +1,22 @@
 from Stopwatch import Stopwatch
 
 
-with Stopwatch("Algorithm"):
-    pass
+# Given an array with the following elements: ["zebra", "macaco", "elefante",
+# "arara", "javali"], after two iterations using bubble sort , what would this
+# array look like?
+
+def bubbleSort(arr):
+    print(arr)
+    iterations = 0
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            iterations += 1
+            if (iterations == 2): print(f"After {iterations} iterations: {arr}")
+    return arr
+
+
+with Stopwatch("Bubble sort"):
+    print(bubbleSort(["zebra", "monkey", "elephant", "macaw", "boar"]))

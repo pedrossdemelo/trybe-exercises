6b42da0a412bfc7ed55b85b0b203bac6e886225e
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-08 03:28:58 -0300

    Import algo

--- Ciência da Computação/Bloco 36 - Algoritmos/Complexidade de Algoritmos/algorithms.py
+++ Ciência da Computação/Bloco 36 - Algoritmos/Complexidade de Algoritmos/algorithms.py
@@ -53,3 +53,10 @@ def random_array(size):
     return array
 # Time complexity: O(n)
 # Space complexity: O(n)
+
+def kids_with_candies(candies, extra_candies):
+    max_candies = max(candies)
+    return [candy + extra_candies >= max_candies for candy in candies]
+
+
+print(kids_with_candies([2, 3, 5, 1, 3], 3))

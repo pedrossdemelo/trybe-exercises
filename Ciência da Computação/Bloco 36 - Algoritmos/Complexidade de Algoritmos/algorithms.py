1d27069874b3e0b934957776534b26708bc151f8
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-08 02:39:51 -0300

    Comment worst, average and best case scenario of time complexy

--- Ciência da Computação/Bloco 36 - Algoritmos/Complexidade de Algoritmos/algorithms.py
+++ Ciência da Computação/Bloco 36 - Algoritmos/Complexidade de Algoritmos/algorithms.py
@@ -1,8 +1,22 @@
 def contains_duplicate(numbers):
-    numbers.sort()
+    numbers.sort() # O(n log n)
     previous_number = 'not a number';
-    for number in numbers:
+    for number in numbers: # O(n)
         if(previous_number == number): return True
         previous_number = number
 
     return False
+
+# Time complexity: O(n log n)
+
+# Best case scenario:
+# Array is already sorted and the duplicate is at the second index
+# Time complexity is O(2)
+
+# Average case scenario:
+# Array is unsorted and the duplicate is at the middle index
+# Time complexity is O(n log n)
+
+# Worst case scenario:
+# Array is unsorted and the duplicate is at the last index
+# Time complexity is O(n log n)

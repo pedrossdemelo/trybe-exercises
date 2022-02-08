b1cc59a8bcf15de9908ec3cfadb9129ea03f2695
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-08 18:14:23 -0300

    Benchmark merge sort vs quick sort in a randomized array of n = 10000

--- Ciência da Computação/Bloco 36 - Algoritmos/Algoritmos de ordenação e busca/Algorithms.py
+++ Ciência da Computação/Bloco 36 - Algoritmos/Algoritmos de ordenação e busca/Algorithms.py
@@ -92,25 +92,33 @@ def insertion_sort(array):
     return array
 
 
-with Stopwatch("Selection sort"):
-    print(selection_sort(randomized[:])) # Randomized n=10000. Time: 1.87s
+# with Stopwatch("Selection sort"):
+#     print(selection_sort(randomized[:])) # Randomized n=10000. Time: 1.87s
 
 
-with Stopwatch("Selection sort (sorted)"):
-    print(selection_sort(sorted[:])) # Sorted n=10000. Time: 1.87s
+# with Stopwatch("Selection sort (sorted)"):
+#     print(selection_sort(sorted[:])) # Sorted n=10000. Time: 1.87s
 
 
-with Stopwatch("Selection sort (inverse sorted)"):
-    print(selection_sort(inverse_sorted[:])) # Sorted n=10000. Time: 1.97s
+# with Stopwatch("Selection sort (inverse sorted)"):
+#     print(selection_sort(inverse_sorted[:])) # Sorted n=10000. Time: 1.97s
 
 
-with Stopwatch("Insertion sort"):
-    print(insertion_sort(randomized[:])) # Randomized n=10000. Time: 2.02s
+# with Stopwatch("Insertion sort"):
+#     print(insertion_sort(randomized[:])) # Randomized n=10000. Time: 2.02s
 
 
-with Stopwatch("Insertion sort (sorted)"):
-    print(insertion_sort(sorted[:])) # Sorted n=10000. Time: 0.001s
+# with Stopwatch("Insertion sort (sorted)"):
+#     print(insertion_sort(sorted[:])) # Sorted n=10000. Time: 0.001s
 
 
-with Stopwatch("Insertion sort (inverse sorted)"):
-    print(selection_sort(inverse_sorted[:])) # Sorted n=10000. Time: 1.97s
+# with Stopwatch("Insertion sort (inverse sorted)"):
+#     print(selection_sort(inverse_sorted[:])) # Sorted n=10000. Time: 1.97s
+
+
+with Stopwatch("Merge sort"):
+    print(merge_sort(randomized[:])) # Randomized n=10000. Time: 0.05s O(n log n)
+
+
+with Stopwatch("Bubble sort"):
+    print(bubbleSort(randomized[:])) # Randomized n=10000. Time: 6.12s O(n^2)

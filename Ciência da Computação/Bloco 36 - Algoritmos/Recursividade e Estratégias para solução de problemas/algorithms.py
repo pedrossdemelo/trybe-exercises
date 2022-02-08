17fcb5e560f0320608fbb4604b19c983a85ed6c2
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-08 16:01:26 -0300

    Implement the same algorithm, but this time recursively

--- Ciência da Computação/Bloco 36 - Algoritmos/Recursividade e Estratégias para solução de problemas/algorithms.py
+++ Ciência da Computação/Bloco 36 - Algoritmos/Recursividade e Estratégias para solução de problemas/algorithms.py
@@ -1,3 +1,10 @@
 # Create a non-recursive algorithm to count how many even numbers there are in a number sequence (1 to n).
 
 def count_evens(nums): return len([x for x in nums if x % 2 == 0])
+
+# Implement the same algorithm, but this time recursively.
+
+def count_evens_recursive(nums):
+    if len(nums) == 0: return 0
+    if nums[0] % 2 == 0: return 1 + count_evens_recursive(nums[1:])
+    else: return count_evens_recursive(nums[1:])

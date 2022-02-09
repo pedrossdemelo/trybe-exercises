6e9a277564d642ac936b33bf62ae15fe118bbff8
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-09 00:37:20 -0300

    Benchmark execution time for 200 numbers without load

--- Ciência da Computação/Bloco 37 - Estrutura de Dados I - Arrays, Hashmaps e Sets/Arquitetura de Computadores/operating_system.py
+++ Ciência da Computação/Bloco 37 - Estrutura de Dados I - Arrays, Hashmaps e Sets/Arquitetura de Computadores/operating_system.py
@@ -17,9 +17,9 @@ def timer(string, initial_time):
 
 # Desempenho da memória primária
 main_load_init_time = perf_counter()
-for number in RANDOM_NUMBERS:
-    main.load(number)
-timer("Números carregados na memória principal", main_load_init_time)
+# for number in RANDOM_NUMBERS:
+#     main.load(number)
+# timer("Números carregados na memória principal", main_load_init_time)
 # Time: 0.000055s for 100 numbers
 # Time: 0.000102s for 200 numbers
 # Time: 0.000089s for 200 numbers without cleanup
@@ -30,19 +30,21 @@ timer(f"Soma na memória principal: {main_sum}", main_get_init_time)
 # Time: 0.000047s for 100 numbers
 # Time: 0.000102s for 200 numbers
 # Time: 0.000099s for 200 numbers without cleanup
+# Time: 0.000064s for 200 numbers without load
 
-# main_clean_init_time = perf_counter()
-# main.clean()
-# timer("Clean na memória principal", main_clean_init_time)
+main_clean_init_time = perf_counter()
+main.clean()
+timer("Clean na memória principal", main_clean_init_time)
 # Time: 0.000003s for 100 numbers 
 # Time: 0.000004s for 200 numbers 
+# Time: 0.000002s for 200 numbers without load
 
 
 # Desempenho da memória secundária
 secondary_load_initial_time = perf_counter()
-for number in RANDOM_NUMBERS:
-    secondary.load(number)
-timer("Números carregados na memória secundária", secondary_load_initial_time)
+# for number in RANDOM_NUMBERS:
+#     secondary.load(number)
+# timer("Números carregados na memória secundária", secondary_load_initial_time)
 # Time: 0.029877s for 100 numbers
 # Time: 0.087865s for 200 numbers
 # Time: 0.324601s for 200 numbers without cleanup
@@ -52,10 +54,12 @@ secondary_sum = sum([secondary.get(i) for i in range(len(RANDOM_NUMBERS))])
 timer(f"Soma na memória secundária: {secondary_sum}", secondary_get_init_time)
 # Time: 0.003471s for 100 numbers
 # Time: 0.007136s for 200 numbers
+# Time: 0.076750s for 200 numbers without cleanup
+# Time: 0.023742s for 200 numbers without load
 
-# secondary_clean_init_time = perf_counter()
-# secondary.clean()
-# timer("Clean na memória secundária", secondary_clean_init_time)
+secondary_clean_init_time = perf_counter()
+secondary.clean()
+timer("Clean na memória secundária", secondary_clean_init_time)
 # Time: 0.006941s for 100 numbers
 # Time: 0.020439s for 200 numbers
-# Time: 0.024360s for 200 numbers without cleanup
+# Time: 0.092397s for 200 numbers without load

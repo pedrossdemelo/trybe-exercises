1005780c3ef1fabc4e87374bf5bd8cad600a7562
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-09 15:12:14 -0300

    Import algorithm instructions

---
+++ Ciência da Computação/Bloco 37 - Estrutura de Dados I - Arrays, Hashmaps e Sets/Set/algorithms.py
@@ -0,0 +1,37 @@
+# Bluff is a two person game where each person tries to guess the numbers that
+# the other will choose. Each player chooses 5 numbers from 0 to 10, inclusive.
+# The final score is calculated as follows:
+# Each person's starting score is the highest number that the other person did not pick;
+# The reducer is the lowest number the other person didn't pick;
+# The final score is the starting score - reducer.
+# Example:
+# Clara = [0, 1, 5, 9, 10]
+# # starting score: 5
+# # reducer: 1
+# # pt: 4
+# Marco = [0, 2, 8, 9, 10]
+# # starting score: 8
+# # Reduction: 2
+# # individual score: 6
+# # Winner: Marco
+# Implement a function that receives a dictionary with the kicked names and
+# numbers and returns the name of the winner. 
+# input = {
+#     'Clara': [0, 1, 5, 9, 10],
+#     'Marco': [0, 2, 8, 9, 10]
+# }
+# output: 'Marco'
+
+
+def winner():
+    # Code here
+    pass
+
+
+# Given a string, find the size of the largest substring that has no repeated
+# characters. Acceptable time limit complexity: O(n^2).
+
+
+def largest_non_repeated_substring(string):
+    # Code here
+    pass

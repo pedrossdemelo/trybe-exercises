8fa07404ff9018be4294e2ad94094fd38ec41e48
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-09 01:47:59 -0300

    Create a script to print available memory every second

--- Ciência da Computação/Bloco 37 - Estrutura de Dados I - Arrays, Hashmaps e Sets/Arquitetura de Computadores/resources.py
+++ Ciência da Computação/Bloco 37 - Estrutura de Dados I - Arrays, Hashmaps e Sets/Arquitetura de Computadores/resources.py
@@ -1,28 +1,38 @@
+import time
 import platform
 from subprocess import check_output
 
+def print_info():
+    if platform.system() == "Linux":
+        print(check_output(["lscpu"]).decode('utf-8'))
+        # print how many cores in the cpu
+        print(check_output(["nproc"]).decode('utf-8'))
+        # print clock speed in MHz
+        print(check_output(["lscpu", "-p", "|", "grep", "CPU MHz"]).decode('utf-8'))
+        # print cache memory
+        print(check_output(["lscpu", "-p", "|", "grep", "L1 cache"]).decode('utf-8'))
+        # print memory info
+        print(check_output(["cat", "/proc/meminfo"]).decode('utf-8'))
+        # print available memory
+        print(check_output(["free", "-m"]).decode('utf-8'))
+        # print used memory
+        print(check_output(["free", "-m", "-t"]).decode('utf-8'))
+    elif platform.system() == "Darwin":
+        print(check_output(["sysctl", "-n", "machdep.cpu.brand_string"]).decode('utf-8'))
+        # print how many cores in the cpu
+        print(check_output(["sysctl", "-n", "hw.physicalcpu"]).decode('utf-8'))
+        # print clock speed in MHz
+        print(check_output(["sysctl", "hw.cpufrequency_max"]).decode('utf-8'))
+        # print cache memory
+        print(check_output(["sysctl", "-n", "hw.l1icachesize"]).decode('utf-8'))
+        # print memory info
+        print(check_output("top -l 1 | head -n 10 | grep PhysMem", shell=True).decode('utf-8'))
 
-if platform.system() == "Linux":
-    print(check_output(["lscpu"]).decode('utf-8'))
-    # print how many cores in the cpu
-    print(check_output(["nproc"]).decode('utf-8'))
-    # print clock speed in MHz
-    print(check_output(["lscpu", "-p", "|", "grep", "CPU MHz"]).decode('utf-8'))
-    # print cache memory
-    print(check_output(["lscpu", "-p", "|", "grep", "L1 cache"]).decode('utf-8'))
-    # print memory info
-    print(check_output(["cat", "/proc/meminfo"]).decode('utf-8'))
-    # print available memory
-    print(check_output(["free", "-m"]).decode('utf-8'))
-    # print used memory
-    print(check_output(["free", "-m", "-t"]).decode('utf-8'))
-elif platform.system() == "Darwin":
-    print(check_output(["sysctl", "-n", "machdep.cpu.brand_string"]).decode('utf-8'))
-    # print how many cores in the cpu
-    print(check_output(["sysctl", "-n", "hw.physicalcpu"]).decode('utf-8'))
-    # print clock speed in MHz
-    print(check_output(["sysctl", "hw.cpufrequency_max"]).decode('utf-8'))
-    # print cache memory
-    print(check_output(["sysctl", "-n", "hw.l1icachesize"]).decode('utf-8'))
-    # print memory info
-    print(check_output("top -l 1 | head -n 10 | grep PhysMem", shell=True).decode('utf-8'))
+
+def get_memory_usage():
+    while True:
+        if platform.system() == "Linux":
+            print(check_output(["free", "-m"]).decode('utf-8'))
+        elif platform.system() == "Darwin":
+            print(check_output("top -l 1 | head -n 10 | grep PhysMem", shell=True).decode('utf-8'))
+        time.sleep(1)

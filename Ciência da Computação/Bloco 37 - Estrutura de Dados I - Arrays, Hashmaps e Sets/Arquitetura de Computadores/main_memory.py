3e1648675446a483787854601518625ffe5d5018
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-08 23:10:31 -0300

    Import code and setup venv

---
+++ Ciência da Computação/Bloco 37 - Estrutura de Dados I - Arrays, Hashmaps e Sets/Arquitetura de Computadores/main_memory.py
@@ -0,0 +1,14 @@
+class MainMemory:
+    def __init__(self):
+        self.clean()
+
+    def load(self, value):
+        # Sua implementação
+        pass
+
+    def get(self, index):
+        pass
+        # Sua implementação
+
+    def clean(self):
+        self.loaded_memory = []

8a4026ca95ac3290013cc836ab2fbc99628de695
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-09 00:13:19 -0300

    Benchmark execution time for 100 numbers for memory and storage

--- Ciência da Computação/Bloco 37 - Estrutura de Dados I - Arrays, Hashmaps e Sets/Arquitetura de Computadores/operating_system.py
+++ Ciência da Computação/Bloco 37 - Estrutura de Dados I - Arrays, Hashmaps e Sets/Arquitetura de Computadores/operating_system.py
@@ -7,7 +7,7 @@ main = MainMemory()
 secondary = SecondaryMemory()
 
 # Numeros aleatórios a serem somados
-RANDOM_NUMBERS = ["36912", "84300"] * 100
+RANDOM_NUMBERS = [36912, 84300] * 100
 
 def timer(string, initial_time):
     """Função auxiliar para print temporizado"""
@@ -20,14 +20,17 @@ main_load_init_time = perf_counter()
 for number in RANDOM_NUMBERS:
     main.load(number)
 timer("Números carregados na memória principal", main_load_init_time)
+# Time: 0.000055s for 100 numbers
 
 main_get_init_time = perf_counter()
 main_sum = sum([main.get(i) for i in range(len(RANDOM_NUMBERS))])
 timer(f"Soma na memória principal: {main_sum}", main_get_init_time)
+# Time: 0.000047s for 100 numbers
 
 main_clean_init_time = perf_counter()
 main.clean()
 timer("Clean na memória principal", main_clean_init_time)
+# Time: 0.000003s for 100 numbers 
 
 
 # Desempenho da memória secundária
@@ -35,11 +38,14 @@ secondary_load_initial_time = perf_counter()
 for number in RANDOM_NUMBERS:
     secondary.load(number)
 timer("Números carregados na memória secundária", secondary_load_initial_time)
+# Time: 0.029877s for 100 numbers
 
 secondary_get_init_time = perf_counter()
 secondary_sum = sum([secondary.get(i) for i in range(len(RANDOM_NUMBERS))])
 timer(f"Soma na memória secundária: {secondary_sum}", secondary_get_init_time)
+# Time: 0.003471s for 100 numbers
 
 secondary_clean_init_time = perf_counter()
 secondary.clean()
 timer("Clean na memória secundária", secondary_clean_init_time)
+# Time: 0.006941s for 100 numbers

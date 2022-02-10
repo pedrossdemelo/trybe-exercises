cc331aa4f4848f43c11fc0cdd63930ca1d11ee57
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-10 02:49:11 -0300

    Create DoublyLinkedList class and implement method to delete duplicates

--- Ciência da Computação/Bloco 38 - Estrutura de Dados II - Listas, Filas e Pilhas/Nó e Listas Encadeadas/Node.py
+++ Ciência da Computação/Bloco 38 - Estrutura de Dados II - Listas, Filas e Pilhas/Nó e Listas Encadeadas/Node.py
@@ -5,3 +5,12 @@ class Node:
 
     def __str__(self):
         return f"Node(value={self.value}, next={self.next})"
+
+
+class DoublyLinkedNode(Node):
+    def __init__(self, value):
+        super().__init__(value)
+        self.previous = None
+
+    def __str__(self):
+        return f"DoublyLinkedNode(value={self.value}, next={self.next})"

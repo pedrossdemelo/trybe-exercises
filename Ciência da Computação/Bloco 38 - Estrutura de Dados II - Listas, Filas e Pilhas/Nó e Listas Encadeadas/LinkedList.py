921403a5a5e3cb54c5de80c675f6f194b740d2f1
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-10 01:13:54 -0300

    Update LinkedList methods to use __get_node_at

--- Ciência da Computação/Bloco 38 - Estrutura de Dados II - Listas, Filas e Pilhas/Nó e Listas Encadeadas/LinkedList.py
+++ Ciência da Computação/Bloco 38 - Estrutura de Dados II - Listas, Filas e Pilhas/Nó e Listas Encadeadas/LinkedList.py
@@ -18,9 +18,10 @@ class LinkedList:
 
     def __get_node_at(self, position):
         if position < 1: return self.head_value
+        if position >= len(self): return None
         value_to_be_returned = self.head_value
         if value_to_be_returned:
-            while position > 1 and value_to_be_returned.next:
+            while position > 0 and value_to_be_returned.next:
                 value_to_be_returned = value_to_be_returned.next
                 position -= 1
         return value_to_be_returned
@@ -32,30 +33,19 @@ class LinkedList:
         self.__length += 1
 
     def insert_last(self, value):
-        last_value = Node(value)
-        current_value = self.head_value
-
-        if self.is_empty():
-            return self.insert_first(value)
-
-        while current_value.next:
-            current_value = current_value.next
-        current_value.next = last_value
-        self.__length += 1
+        return self.insert_at(value, len(self))
 
     def insert_at(self, value, position):
         if position < 1:
             return self.insert_first(value)
-        if position >= len(self):
+        if position > len(self):
             return self.insert_last(value)
-        current_value = self.head_value
-        while position > 1:
-            current_value = current_value.next
-            position -= 1
-        next_value = Node(value)
-        next_value.next = current_value.next
-        current_value.next = next_value
+        previous_to_be_inserted = self.__get_node_at(position - 1)
+        new_value = Node(value)
+        new_value.next = previous_to_be_inserted.next
+        previous_to_be_inserted.next = new_value
         self.__length += 1
+        return
 
     def remove_first(self):
         value_to_be_removed = self.head_value
@@ -68,46 +58,23 @@ class LinkedList:
     def remove_last(self):
         if len(self) <= 1:
             return self.remove_first()
-
-        previous_to_be_removed = self.head_value
-
-        while previous_to_be_removed.next.next:
-            previous_to_be_removed = previous_to_be_removed.next
-
-        value_to_be_removed = previous_to_be_removed.next
-        previous_to_be_removed.next = None
-        self.__length -= 1
-        return value_to_be_removed
+        return self.remove_at(len(self))
 
     def remove_at(self, position):
         if position < 1:
             return self.remove_first()
-        if position >= len(self):
+        if position > len(self):
             return self.remove_last()
 
-        previous_to_be_removed = self.head_value
-
-        while position > 1:
-            previous_to_be_removed = previous_to_be_removed.next
-            position -= 1
-
+        previous_to_be_removed = self.__get_node_at(position - 1)
         value_to_be_removed = previous_to_be_removed.next
         previous_to_be_removed.next = value_to_be_removed.next
-        value_to_be_removed.next = None
         self.__length -= 1
 
         return value_to_be_removed
 
     def get_element_at(self, position):
-        value_returned = None
-        value_to_be_returned = self.head_value
-        if value_to_be_returned:
-            while position > 0 and value_to_be_returned.next:
-                value_to_be_returned = value_to_be_returned.next
-                position -= 1
-            if value_to_be_returned:
-                value_returned = Node(value_to_be_returned.value)
-        return value_returned
+        return Node(self.__get_node_at(position).value)
 
     def is_empty(self):
         return not self.__length
@@ -143,3 +110,17 @@ if __name__ == "__main__":
     linked_list.insert_at(8, 3)
     linked_list.insert_at(9, 4)
     print(linked_list.get_element_at(3)) # output: Node(value=8 next=None)
+
+    linked_list.clear()
+    for i in range(10):
+        linked_list.insert_first(1)
+
+    print(linked_list)
+    linked_list.insert_at(69, 3)
+    print(linked_list)
+    for i in range(10):
+        print(linked_list.get_element_at(i))
+    print(linked_list)
+    linked_list.remove_at(3)
+    linked_list.insert_last(69)
+    print(linked_list)

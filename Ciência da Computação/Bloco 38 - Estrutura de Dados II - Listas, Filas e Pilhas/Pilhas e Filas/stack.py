dde7cf441c34d8666f25c80dd0b4a94588aa5075
Pedro Sousa <pedrossm2000@gmail.com>
2022-02-10 15:07:22 -0300

    Implement Stack using only arrays instead of linkedlists

---
+++ Ciência da Computação/Bloco 38 - Estrutura de Dados II - Listas, Filas e Pilhas/Pilhas e Filas/stack.py
@@ -0,0 +1,61 @@
+class Stack():
+    def __init__(self):
+        self._data = list()
+
+    def size(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return not bool(self.size())
+
+    def push(self, value):
+        self._data.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+
+        # -1 se refere ao último objeto da pilha.
+        # Ou seja, o valor do topo da pilha
+        value = self._data[-1]
+        del self._data[-1]
+        return value
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        value = self._data[-1]
+        return value
+
+    def clear(self):
+        self._data.clear()
+
+    def __str__(self):
+        str_items = ""
+        for i in range(self.size()):
+            value = self._data[i]
+            str_items += str(value)
+            if i + 1 < self.size():
+                str_items += ", "
+
+        return "Stack(" + str_items + ")"
+
+
+if __name__ == "__main__":
+    elements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    content_stack = Stack()
+
+    for elem in elements:
+        content_stack.push(elem)
+
+    print(content_stack) # output: Stack(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    print(content_stack.size()) # output: 10
+
+    print(content_stack.peek()) # output: 10
+    print(content_stack.pop()) # output: 10, the function returns the item being removed
+
+    print(content_stack.peek()) # output: 9, the new top of the stack
+    print(content_stack.size()) # output: 9
+
+    print(content_stack.clear()) # output: None, the stack was cleared
+    print(content_stack.size()) # output: 0
